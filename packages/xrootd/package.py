45a0b6d8b5a35e182acd57263cd5db6cc671103a
Brett Viren <brett.viren@gmail.com>
2016-04-12 18:59:57 -0400

    Add initial XRootD packaging.

---
+++ packages/xrootd/package.py
@@ -0,0 +1,25 @@
+from spack import *
+
+class Xrootd(Package):
+    """The XROOTD project aims at giving high performance, scalable fault tolerant access to data repositories of many kinds."""
+    homepage = "http://xrootd.org"
+    url      = "http://xrootd.org/download/v4.3.0/xrootd-4.3.0.tar.gz"
+
+    version('4.3.0', '39c2fab9f632f35e12ff607ccaf9e16c')
+
+
+    def install(self, spec, prefix):
+        options = []
+        options.extend(std_cmake_args)
+
+        build_directory = join_path(self.stage.path, 'spack-build')
+        source_directory = self.stage.source_path
+
+        if '+debug' in spec:
+            options.append('-DCMAKE_BUILD_TYPE:STRING=Debug')
+
+        with working_dir(build_directory, create=True):
+            cmake(source_directory, *options)
+            make()
+            make("install")
+
