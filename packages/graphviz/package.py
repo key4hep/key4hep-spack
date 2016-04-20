2014c4503eb4c065ad676ee7283ce910377e009d
Patrick Gartung <gartung@fnal.gov>
2016-04-20 10:57:45 -0500

    the original url times out

--- packages/graphviz/package.py
+++ packages/graphviz/package.py
@@ -5,8 +5,7 @@ from spack import *
 class Graphviz(Package):
     """Graph Visualization Software"""
     homepage = "http://www.graphviz.org"
-    url      = "http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.38.0.tar.gz"
-
+    url = "http://pkgs.fedoraproject.org/repo/pkgs/graphviz/graphviz-2.38.0.tar.gz/5b6a829b2ac94efcd5fa3c223ed6d3ae/graphviz-2.38.0.tar.gz"
     version('2.38.0', '5b6a829b2ac94efcd5fa3c223ed6d3ae')
 
     parallel = False
