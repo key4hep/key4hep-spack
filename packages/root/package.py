5cfcd07606b5c81c533f2ba314f7905f890e26d9
Patrick Gartung <gartung@fnal.gov>
2017-01-12 13:49:04 -0600

    add root 6.08 which needs libtool?

--- packages/root/package.py
+++ packages/root/package.py
@@ -32,6 +32,7 @@ class Root(Package):
     homepage = "https://root.cern.ch"
     url      = "https://root.cern.ch/download/root_v6.07.02.source.tar.gz"
 
+    version('6.08.02', '50c4dbb8aa81124aa58524e776fd4b4b')
     version('6.06.08', '6ef0fe9bd9f88f3ce8890e3651142ee4')
     version('6.06.06', '4308449892210c8d36e36924261fea26')
     version('6.06.04', '55a2f98dd4cea79c9c4e32407c2d6d17')
@@ -44,6 +45,7 @@ class Root(Package):
     variant('graphviz', default=False, description='Enable graphviz support')
 
     depends_on("cmake", type='build')
+    depends_on("libtool", type='build')
     depends_on("pcre")
     depends_on("fftw")
     depends_on("graphviz", when="+graphviz")
@@ -55,6 +57,7 @@ class Root(Package):
         depends_on("libpng")
         depends_on("openssl")
         depends_on("freetype")
+        depends_on("xz")
 
     def install(self, spec, prefix):
         build_directory = join_path(self.stage.path, 'spack-build')
