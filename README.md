c226f1d5dca6bc695cd0f94e0e2517f035ad3b65
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-10 13:17:52 +0100

    Initial commit of k4 packages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [Spack](https://github.com/LLNL/spack) package repo for HEP software packaging
+# [Spack](https://github.com/LLNL/spack) package repo for Key4HEP software packaging
 
 This holds a set of Spack packages for common HEP software.  It relies
 on Spack and the builtin Spack packages, some of which are overridden
@@ -12,10 +12,10 @@ Initial setup like:
 cd /path/to/big/disk
 git clone https://github.com/LLNL/spack.git
 cd spack/var/spack/repos
-git clone https://github.com/HEP-SF/hep-spack.git
+git clone https://github.com/key4hep/k4-spack.git
 cd -
 ./spack/bin/spack compiler add /usr/bin/gcc
-./spack/bin/spack repo add spack/var/spack/repos/hep-spack
+./spack/bin/spack repo add spack/var/spack/repos/k4-spack
 ```
 
 To not have to type a full path to `spack` and to gain some other shell-level features do
@@ -29,11 +29,11 @@ This is assumed below.
 
 ## Some Exercises
 
-### Install ROOT
+### Install EDM4hep
 
 ```bash
-$ spack info root
-$ spack install root
+$ spack info edm4hep
+$ spack install edm4hep
 ```
 
 ### Setup environment
@@ -41,57 +41,4 @@ $ spack install root
 If you have [Environment Modules](http://modules.sf.net) installed:
 
 ```bash
-$ spack load root
-$ root
-   --------------------------------------------------------------------------
-  | Welcome to ROOT 6.07/02                              http://root.cern.ch |
-  |                                             (c) 1995-2014, The ROOT Team |
-  | Built for linuxx8664gcc                                                  |
-  | From heads/master@v6-07-01-ROOTaaS-1-123-gb5924cd, Dec 18 2015, 11:21:36 |
-  | Try '.help', '.demo', '.license', '.credits', '.quit'/'.q'               |
-   --------------------------------------------------------------------------
-```
-
-
-### Create Geant4 packaging
-
-
-```bash
-$ spack create -r /path/to/big/disk/hep-spack -N hsf -n geant4 http://geant4.cern.ch/support/source/geant4.10.01.p03.tar.gz
-```
-
-Your `$EDITOR` will open.  Close the file to continue.  Later, you
-locate the file manually or do:
-
-```bash
-$ spack edit geant4
-$ spack install geant4
-```
-
-This fails first time failing to download Xerces-C:
-
-```bash
-$ spack versions xerces-c
-==> Safe versions (already checksummed):
-  3.1.2
-==> Remote versions (not yet checksummed):
-  256  5  3.1.3  1  c
-$ spack checksum xerces-c@3.1.3
-...
-How many would you like to checksum? (default is 5, q to abort) 3
-...
-      version('3.1.3', '70320ab0e3269e47d978a6ca0c0e1e2d')
-$ spack edit xerces-c
-```
-
-Paste that `version` line into the `xerces-c` package.
-
-```bash
-$ spack install geant4
-...
-==> Successfully installed geant4.
-  Fetch: 0.00s.  Build: 11m 51.38s.  Total: 11m 51.38s.
-```
-
-Now this picks up the newer Xerces-C 3.1.3, builds it and starts
-building Geant4.
+$ spack load edm4hep
---
+++ packages/edm4hep/package.py
@@ -0,0 +1,81 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the LICENSE file for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+
+from spack import *
+
+
+class EDM4hep(CMakePackage):
+    """Event data model of Key4HEP"""
+
+    homepage = "https://github.com/HSF/EDM4hep"
+    git = "https://github.com/HSF/EDM4hep.git"
+
+    version('master', branch='master')
+
+    variant('build_type', default='Release',
+            description='The build type to build',
+            values=('Debug', 'Release'))
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    depends_on('cmake', type='build')
+    depends_on('python', type='build')
+    depends_on('root')
+    depends_on('podio')
+
+    # in LCG_96 ROOT is installed with an external xz rather than the builtin,
+    # so the genreflex binary needs to find it.
+    # As root is installed as an external package we cannot modify its
+    # setup_dependent_environment function to add the xz lib folder to the
+    # LD_LIBRARY_PATH hence we need to do it here.
+    depends_on('xz', when='^root@6.16:')
+
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+        return args
+
+    # Override pre-defined test step
+    # Multiple tests access to the same root file, thus we avoid parallel
+    # execution at this stage
+    def check(self):
+        with working_dir(self.build_directory):
+            make("test", "CTEST_OUTPUT_ON_FAIL=1")
+
+    def setup_environment(self, spack_env, run_env):
+	# needed for genreflex
+    	spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+    	run_env.prepend_path('LD_LIBRARY_PATH', self.spec['root'].prefix.lib)
+        if 'xz' in self.spec:
+            spack_env.prepend_path('LD_LIBRARY_PATH', self.spec['xz'].prefix.lib)
+
+    def setup_dependent_environment(self, spack_env, run_env, dspec):
+        #spack_env.set('EDM4hep', self.prefix)
+        pass
