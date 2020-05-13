cc012b6728e27e2548a6078c9d3023ca970976cf
Valentin Volkl <valentin.volkl@cern.ch>
2020-05-13 12:32:12 +0200

    change lcio defaults and update key4hep-stack

--- packages/key4hep-stack/package.py
+++ packages/key4hep-stack/package.py
@@ -9,14 +9,27 @@ class Key4hepStack(BundlePackage):
     
     version('0.1')
     version('nightly')
+
     
     depends_on('podio@master', when="@nightly")
     depends_on('edm4hep@master', when="@nightly")
     depends_on('K4FWCore@master', when="@nightly")
     depends_on('guinea-pig@master', when="@nightly")
-    depends_on('dd4hep@master', when='@nightly')
     
     depends_on("edm4hep@0.1.0", when="@0.1")
     depends_on("K4FWCore@0.1.0", when="@0.1")
     depends_on('guinea-pig@1.2.2rc', when="@0.1")
+
+    depends_on('lcio')
+    depends_on('lcgeo')
+
+    # 
+    depends_on('root cxxstd=17 +root7 +ssl')
+    depends_on('boost +python')
+
+    # there are known issues with compilers from red hat's devtoolset
+    # which are therefore not supported
+    # https://root-forum.cern.ch/t/devtoolset-gcc-toolset-compatibility/38286
+    conflicts("gcc@8.3.1")
+
     
