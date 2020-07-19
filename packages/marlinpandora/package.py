8f57eefa5808315810b66e06252a76ac6a1eba32
Valentin Volkl <valentin.volkl@cern.ch>
2020-07-19 17:48:35 +0200

    [ilcsoft]

---
+++ packages/marlinpandora/package.py
@@ -0,0 +1,32 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+from spack.pkg.k4.Ilcsoftpackage import ilc_url_for_version
+
+
+class Marlinpandora(CMakePackage):
+    """Assembly of various Marlin processor for reconstruction."""
+
+    url      = "https://github.com/PandoraPFA/MarlinPandora/archive/v03-00-01.tar.gz"
+    homepage = "https://github.com/PandoraPFA/MarlinPandora"
+    git      = "https://github.com/PandoraPFA/MarlinPandora.git"
+
+    maintainers = ['vvolkl']
+
+    version('master', branch='master')
+    version('3.0.1', sha256='2caecf5aa804dc0a0e2e4d6e87ad9100f76eafd1fd258f73130a9b476f9a4378')
+
+
+    depends_on('ilcutil')
+    depends_on('marlin')
+    depends_on('marlinutil')
+    depends_on('pandorapfa')
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libMarlinPandora.so")
+
+    def url_for_version(self, version):
+       return ilc_url_for_version(self, version)
---
+++ packages/pandoraanalysis/package.py
@@ -0,0 +1,34 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+from spack.pkg.k4.Ilcsoftpackage import ilc_url_for_version
+
+
+class Pandoraanalysis(CMakePackage):
+    """Pandora calibration and analysis tools in iLCSoft / Marlin framework"""
+
+    url      = "https://github.com/PandoraPFA/LCPandoraAnalysis/archive/v02-00-01.tar.gz"
+    homepage = "https://github.com/PandoraPFA/LCPandoraAnalysis"
+    git      = "https://github.com/PandoraPFA/LCPandoraAnalysis.git"
+
+    maintainers = ['vvolkl']
+
+    version('master', branch='master')
+    version('2.0.1', sha256='cab082096921d60390054bb0da6afc5eaee4df28411266d4404f9b3f50048e39')
+
+
+    depends_on('ilcutil')
+    depends_on('marlin')
+    depends_on('marlinutil')
+    depends_on('lcio')
+    depends_on('root')
+    depends_on('dd4hep')
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libPandoraAnalysis.so")
+
+    def url_for_version(self, version):
+       return ilc_url_for_version(self, version)
