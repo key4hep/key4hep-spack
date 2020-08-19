2ed67df4b03bdf05531a4eb3b16602684404a399
Valentin Volkl <valentin.volkl@cern.ch>
2020-08-19 13:13:47 +0200

    [fcc-edm] new version

--- packages/fcc-edm/package.py
+++ packages/fcc-edm/package.py
@@ -9,6 +9,7 @@ class FccEdm(CMakePackage):
     git =      "https://github.com/HEP-FCC/fcc-edm.git"
 
     version('master', branch='master')
+    version('0.5.7', sha256='8fe7f45014a401d635c00bad12c8fd251e2cabbabdfea0304c06c3ac926c60ca')
     version('0.5.6', sha256='aaf4ff58dfbdf9dfc3f755ad8b14d5e5701ed875f4031b1f7538deaf0f027705')
     version('0.5.5', sha256='a07a2f1304ce08a6d9819200c77e4a739f1e96f2ebb59715ebc27992e6a014e0')
     version('0.5.4', '236206ca4e00f239d574bfcd6aa44b53')
@@ -28,11 +29,11 @@ class FccEdm(CMakePackage):
 
     variant('cxxstd',
             default='17',
-            values=('14', '17'),
+            values=('17'),
             multi=False,
             description='Use the specified C++ standard when building.')
 
-    patch('cpack.patch')
+    patch('cpack.patch', when="@:0.5.6")
 
     depends_on('cmake', type='build')
     depends_on('python', type='build')
@@ -53,6 +54,3 @@ class FccEdm(CMakePackage):
     def check(self):
         with working_dir(self.build_directory):
             make("test", "CTEST_OUTPUT_ON_FAIL=1")
-
-    def setup_run_environment(self, spack_env):
-        spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include.datamodel)
