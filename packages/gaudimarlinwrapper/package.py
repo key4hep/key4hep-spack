4989f748be946e5594065c37c4832ee1a49522d3
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-09-21 15:46:26 +0200

    GMP: add version 0.2.1

--- packages/gaudimarlinwrapper/package.py
+++ packages/gaudimarlinwrapper/package.py
@@ -16,6 +16,7 @@ class Gaudimarlinwrapper(CMakePackage):
     maintainers = ['fdplacido']
 
     version('master', branch='master')
+    version('0.2.1', sha256='f1d50c41091954b94713573fe6dae7baf95607f2eed9b923dfa91738d3a399a6')
     version('0.2', sha256='cdadc1f33399c999c8beef24c72b1ad4ccadc11c522ffb77f8ddcf0896773da3')
     version('0.1', sha256='649ffa462e75c716f54b4e0b530c27c27a7f62a8fad64af8aecc646046045efd')
 
