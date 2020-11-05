df7d39ff3c192cc63ec93404b8146ef010605660
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-05 12:10:39 +0100

    add fccanalyses

---
+++ packages/fccanalyses/package.py
@@ -0,0 +1,14 @@
+
+class Fccanalyses(BundlePackage):
+  """ Analysis for the FCC. This package is usually built locally, but could be installed with spack (todo). """
+
+  homepage = 'https://github.com/HEP-FCC/FCCAnalyses'
+
+  version('main')
+
+  depends_on('py-particle')
+  depends_on('py-awkward1')
+  depends_on('py-matplotlib')
+  depends_on('py-uproot4')
+  depends_on('py-tensorflow')
+  #depends_on('py-zfit') # todo: add in spack
