800bd896e09271c0f5fd04373b1b9b3984bc33e9
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-23 12:48:25 +0100

    [k4gen] add preliminary version of package (to test ci)

---
+++ packages/k4gen/package.py
@@ -0,0 +1,49 @@
+
+from spack import *
+from spack.pkg.k4.Ilcsoftpackage import Key4hepPackage, k4_add_latest_commit_as_version 
+
+class K4gen(CMakePackage, Key4hepPackage):
+    """Generator components for the Key4hep framework"""
+    homepage = "https://github.com/HEP-FCC/k4Gen/"
+    url      = "https://github.com/HEP-FCC/k4Gen/archive/v0.16.tar.gz"
+    git      = "https://github.com/HEP-FCC/k4Gen.git"
+
+    maintainers = ['vvolkl']
+
+    version('master', branch='master')
+    k4_add_latest_commit_as_version(git)
+
+    variant('build_type', default='Release',
+            description='CMake build type',
+            values=('Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel'))
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    depends_on('fastjet')
+    depends_on("edm4hep")
+
+
+    depends_on('k4fwcore@1:'
+    depends_on('hepmc@:2.99.99')
+    depends_on('heppdt@:2.99.99')
+
+    depends_on('pythia8', when="@:0.12")
+    depends_on('evtgen+pythia8', when="@0.13:")
+
+
+
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+        return args
+
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('PYTHONPATH', self.prefix.python)
+        spack_env.prepend_path("PATH", self.prefix.scripts)
+        spack_env.set("K4GEN", self.prefix.share.k4Gen)
