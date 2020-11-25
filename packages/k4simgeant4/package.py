42a00457bd7347a43b322600f0d787c8a3c9e98a
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-26 00:40:36 +0100

    [k4simgeant4] add preliminary package recipe for testing

---
+++ packages/k4simgeant4/package.py
@@ -0,0 +1,42 @@
+
+from spack import *
+from spack.pkg.k4.Ilcsoftpackage import Key4hepPackage, k4_add_latest_commit_as_version 
+
+class K4simgeant4(CMakePackage, Key4hepPackage):
+    """Software framework of the FCC project"""
+    homepage = "https://github.com/HEP-FCC/k4SimGeant4/"
+    url      = "https://github.com/HEP-FCC/k4SimGeant4/archive/v0.16.tar.gz"
+    git      = "https://github.com/HEP-FCC/k4SimGeant4.git"
+
+    maintainers = ['vvolkl']
+
+    version('main', branch='main')
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    depends_on('clhep')
+    depends_on('dd4hep +geant4')
+    depends_on('k4fwcore@1.0:')
+    depends_on('geant4')
+
+
+    depends_on("g4ensdfstate")
+
+
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s' % self.spec.variants['cxxstd'].value)
+        return args
+
+    def setup_build_environment(self, spack_env):
+        spack_env.set('G4ENSDFSTATEDATA', self.spec["g4ensdfstate"].prefix + "/share/data/G4ENSDFSTATE2.2/")
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('PYTHONPATH', self.prefix.python)
+        spack_env.prepend_path("PATH", self.prefix.scripts)
+        spack_env.set("K4SIMGEANT4", self.prefix.share.k4SimGeant4)
