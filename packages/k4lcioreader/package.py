d4043b9d02769bfa45c1a2a188eb4ed27422c444
lintao <lintao51@gmail.com>
2020-12-01 20:27:55 +0800

    Update k4LCIOReader to v0.2.0. The LCIOInput is merged into this package, so need to depend on k4FWCore.

--- packages/k4lcioreader/package.py
+++ packages/k4lcioreader/package.py
@@ -13,6 +13,7 @@ class K4lcioreader(CMakePackage, Key4hepPackage):
     version('master', branch='master')
     k4_add_latest_commit_as_version(git)
     version('0.1.0', sha256='996d1ff78c0a8a2f7f358dd4ea19f955853ad0902ee86b99c484de58c5fc2e2c')
+    version('0.2.0', sha256='346fc2ba4b4175895597e093f566ba6407be9eeb9cde0766304e0f19ad03e081')
 
     variant('cxxstd',
             default='17',
@@ -24,6 +25,7 @@ class K4lcioreader(CMakePackage, Key4hepPackage):
     depends_on('lcio')
     depends_on('podio@0.12:')
     depends_on('edm4hep')
+    depends_on('k4fwcore')
 
     def cmake_args(self):
         args = []
