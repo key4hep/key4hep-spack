23f02a5b595571cbe92aa566010075c9607d5830
Valentin Volkl <valentin.volkl@cern.ch>
2020-12-02 19:34:11 +0100

    new fccanalyses

--- packages/fccanalyses/package.py
+++ packages/fccanalyses/package.py
@@ -1,14 +1,23 @@
+from spack.pkg.k4.Ilcsoftpackage import Key4hepPackage
 
-class Fccanalyses(BundlePackage):
-  """ Analysis for the FCC. This package is usually built locally, but could be installed with spack (todo). """
+class Fccanalyses(CMakePackage, Key4hepPackage):
+    """ RDF Analysers for the FCC. """
+  
+    homepage = 'https://github.com/HEP-FCC/FCCAnalyses'
+    git = 'https://github.com/HEP-FCC/FCCAnalyses.git'
+    url = 'https://github.com/HEP-FCC/FCCAnalyses/archive/v0.1.1.tar.gz'
 
-  homepage = 'https://github.com/HEP-FCC/FCCAnalyses'
-
-  version('main')
-
-  depends_on('py-particle')
-  depends_on('py-awkward1')
-  depends_on('py-matplotlib')
-  depends_on('py-uproot4')
-  #depends_on('py-tensorflow') # todo: check if we should integrate.
-  #depends_on('py-zfit') # todo: add in spack
+    maintainers = ['vvolkl', 'clementhelsens']
+  
+    version('master', branch='master')
+    version("0.2.0pre01", tag="v0.2.0pre01")
+    
+    depends_on("root")
+    depends_on("vdt")
+    depends_on("fastjet")
+    depends_on('python')
+    depends_on('edm4hep')
+    depends_on('fcc-edm')
+  
+    def setup_run_environment(self, spack_env):
+      spack_env.prepend_path('ROOT_INCLUDE_DIR', self.prefix.include.FCCAnalyses)
