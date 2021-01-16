cb89521d12b1b3ccc6fd53d71a323614f7c42e31
lintao <lintao51@gmail.com>
2021-01-16 11:03:37 +0800

    WIP: add k4FWCore v0.3.0

--- packages/key4hep-stack/package.py
+++ packages/key4hep-stack/package.py
@@ -247,8 +247,8 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
     depends_on("cepcsw")
     k4_add_latest_commit_as_dependency("cepcsw", "cepc/cepcsw", when="@master")
     
-    #depends_on("k4lcioreader")
-    #k4_add_latest_commit_as_dependency("k4lcioreader", "key4hep/k4LCIOReader", when="@master")
+    depends_on("k4lcioreader")
+    k4_add_latest_commit_as_dependency("k4lcioreader", "key4hep/k4LCIOReader", when="@master")
 
 
     ##################### developer tools #################
