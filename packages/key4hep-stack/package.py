1f381d43c2aaad4df223795cdb97814b107ec442
Valentin Volkl <valentin.volkl@cern.ch>
2021-02-09 18:41:41 +0100

    extended comments and clarified code

--- packages/key4hep-stack/package.py
+++ packages/key4hep-stack/package.py
@@ -1,10 +1,9 @@
-from spack import *
-from spack.main import get_version
-import spack.architecture as architecture
 from datetime import datetime
 import os
 import platform
 import llnl.util.tty as tty
+import spack.architecture as architecture
+from spack.main import get_version
 import spack.user_environment as uenv
 from spack.pkg.k4.Ilcsoftpackage import k4_add_latest_commit_as_dependency 
 from spack.pkg.k4.Ilcsoftpackage import k4_generate_setup_script 
@@ -18,22 +17,30 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
 
     ##################### versions ########################
     #######################################################
-    # tries to build the HEAD of each package.
-    # used for master builds
+    ###  nightly builds
+    # builds the HEAD of each package for which 
+    # k4_add_latest_commit_as_dependency is called below
     version('master')
-    # the preferred usage is to use the date as versio, like: 
-    # builds the latest stable version of each package
-    # the preferred usage is to use the date as version, see below
-    #version('master-2020-10-06')
+    # this version can be extended with additional version
+    # fields to differentiate it, like 'master-2020-10-10'
+    #
+    ### stable builds
+    # builds the latest release of each package
+    # the preferred usage is to use the date as version, like:
     version(datetime.today().strftime('%Y-%m-%d'))
-    #version('2020-10-06') # example, no need to add them here
+    #version('2020-10-06') # example, no need to add here.
+    # more complex version configurations should be added in an
+    # environment
 
+    # this bundle package installs a custom setup script, so
+    # need to add the install phase (which normally does not
+    # exist for a bundle package)
     phases = ['install']
 
     ##################### variants ########################
     #######################################################
     variant('devtools', default=True,
-            description="add tools necessary for software masterment to the stack")
+            description="add tools necessary for software development to the stack")
     variant('generators', default=False,
             description="add some standalone generators to the stack")
     variant('bootstrap', default=False,
@@ -66,7 +73,6 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
     k4_add_latest_commit_as_dependency("k4simgeant4", "hep-fcc/k4simgeant4", when="@master",
 				 giturl="https://api.github.com/repos/%s/commits/main")
 
-    
     depends_on("guinea-pig")
     # todo: figure out the api for the cern gitlab instance
     #depends_on('guinea-pig@master', when="@master")
@@ -77,7 +83,6 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
 
     depends_on("delphes")
     k4_add_latest_commit_as_dependency("delphes", "delphes/delphes", when="@master")
-    #depends_on("delphes@master", when="@master")
 
     ##################### general purpose generators ######
     #######################################################
@@ -166,8 +171,7 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
     k4_add_latest_commit_as_dependency("lccd", "ilcsoft/lccd", when="@master")
 
     depends_on('lcio')
-    #k4_add_latest_commit_as_dependency("lcio", "ilcsoft/lcio", when="@master")
-    #depends_on("lcio@master", when="@master")
+    k4_add_latest_commit_as_dependency("lcio", "ilcsoft/lcio", when="@master")
 
     depends_on('lcgeo')
     k4_add_latest_commit_as_dependency("lcgeo", "ilcsoft/lcgeo", when="@master")
@@ -267,7 +271,6 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
     #depends_on('py-zfit') # todo: add in spack
 
 
-
     ##################### environment boostrap ############
     #######################################################
     depends_on("environment-modules", when="+bootstrap")
@@ -280,49 +283,48 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
               "which are therefore not supported." \
               "See https://root-forum.cern.ch/t/devtoolset-gcc-toolset-compatibility/38286")
 
+
     def install(self, spec, prefix):
       """ Create bash setup script in prefix."""
-
       # first, log spack version to build-out
-
       tty.msg('* **Spack:**', get_version())
       tty.msg('* **Python:**', platform.python_version())
       tty.msg('* **Platform:**', architecture.Arch(
           architecture.platform(), 'frontend', 'frontend'))
-
-
-      specs = [spec]
+      # get all dependency specs, including compiler
       with spack.store.db.read_transaction():
-               specs = [dep for _spec in specs
-                        for dep in
-                        _spec.traverse( order='post')]
+               specs = [dep for dep in spec.traverse(order='post')]
                try: 
-                   gcc_specs = [spack.cmd.disambiguate_spec(str(spec.compiler), None, first=True)]
-                   gcc_specs = [dep for _spec in gcc_specs
-                            for dep in
-                            _spec.traverse( order='post')]
+                   gcc_spec = spack.cmd.disambiguate_spec(str(spec.compiler), 
+                                                          None,
+                                                          first=True)
+                   gcc_specs = [dep for dep in gcc_spec.traverse( order='post')]
                    specs = specs + gcc_specs
                except:
-                   tty.warn("No spec found for " + str(spec.compiler) + " Assuming it is a system compiler and not adding it to the setup.")
+                   tty.warn("No spec found for " + str(spec.compiler) +
+                            ". Assuming it is a system compiler,  
+                            not adding it to the setup.")
+      # record all changes to the environment by packages in the stack
       env_mod = spack.util.environment.EnvironmentModifications()
       for _spec in specs:
           env_mod.extend(uenv.environment_modifications_for_spec(_spec))
           env_mod.prepend_path(uenv.spack_loaded_hashes_var, _spec.dag_hash())
+      # transform to bash commands, and write to file
       cmds = k4_generate_setup_script(env_mod)
       with open(os.path.join(prefix, "setup.sh"), "w") as f:
         f.write(cmds)
+        # optionally add a symlink (location configurable via environment variable
+        # K4_LATEST_SETUP_PATH. Step will be skipped if it is empty)
         try:
           symlink_path = os.environ.get("K4_LATEST_SETUP_PATH", "")
           if symlink_path:
+              # make sure that the path exists, create if not
               if not os.path.exists(os.path.dirname(symlink_path)):
                 os.makedirs(os.path.dirname(symlink_path))
+              # make sure that an existing file will be overwritten,
+              # even if it is a symlink (for which 'exists' is false!)
               if os.path.exists(symlink_path) or os.path.islink(symlink_path):
                 os.remove(symlink_path)
               os.symlink(os.path.join(prefix, "setup.sh"), symlink_path)
         except:
           tty.warn("Could not create symlink")
-
-
-   
-
-    
