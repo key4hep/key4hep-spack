54f5cf76ae609a4826a74f2d9c1a22919be1ff4c
Valentin Volkl <valentin.volkl@cern.ch>
2021-02-11 20:13:23 +0100

    [k4simdelphes] update

--- packages/k4simdelphes/package.py
+++ packages/k4simdelphes/package.py
@@ -17,15 +17,36 @@ class K4simdelphes(CMakePackage):
     maintainers = ['vvolkl']
 
     version('main', branch='main')
+    version('0.1.2', sha256='c36a123ace6150c05d4b1114b532cf2a3a1b63e96f706a84bed849fd61f0def7')
     version('0.0.1', sha256='4bc414ac72cd03638e7f406381b41814f6e19f3425051f094ac0b539630cd698')
 
     patch('cmake2.patch', when="@0.0.1")
 
+    variant('framework', default=True, description="Build Gaudi framework integration.")
+    variant('integration_tests', default=True, description="Enable integration tests for framework.")
+    variant('delphes_pythia', default=True, description='Build standalone executable with Pythia input.')
+    variant('delphes_hepmc', default=True, description='Build standalone executable with Hepmc input.')
+    variant('delphes_pythia_evtgen', default=True, description='Build standalone executable with Pythia+EvtGen input')
+
     depends_on('edm4hep')
-    depends_on('delphes@3.4.3pre06:')
-    depends_on('pythia8')
-    depends_on('evtgen+pythia8')
-    depends_on('hepmc')
+    depends_on('delphes@3.4.3pre08:')
+    depends_on('pythia8', when="+delphes_pythia")
+    depends_on('evtgen+pythia8', when="+delphes_pythia_evtgen")
+    depends_on('hepmc', when="+delphes_hepmc")
+    depends_on('k4fwcore', when="+framework")
+
+    depends_on('catch2@3.0.1:', type=('test'))
+    depends_on('k4gen', when="+integration_tests", type=('test'))
+
+    def cmake_args(self):
+        args = [
+            self.define_from_variant("BUILD_FRAMEWORK", 'framework'),
+            self.define_from_variant("BUILD_PYTHIA_READER", 'delphes_pythia'),
+            self.define_from_variant("BUILD_HEPMC_READER", 'delphes_hepmc'),
+            self.define_from_variant("BUILD_EVTGEN_READER", 'delphes_pythia_evtgen'),
+            self.define("USE_EXTERNAL_CATCH2", True),
+        ]
+        return args
 
     def setup_build_environment(self, env):
         env.set('PYTHIA8', self.spec["pythia8"].prefix)
