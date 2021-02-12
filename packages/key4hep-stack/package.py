030dcf3d455b0d2066bfa8fef39922cbd5025f4b
vvolkl <valentin.volkl@cern.ch>
2021-02-12 14:41:40 +0100

    add more python analysis packages to devtools (#160)
    * add more python analysis packages to devtools
    
    * fix typo

--- packages/key4hep-stack/package.py
+++ packages/key4hep-stack/package.py
@@ -267,8 +267,13 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
     depends_on('py-awkward1', when="+devtools")
     depends_on('py-matplotlib', when="+devtools")
     depends_on('py-uproot4', when="+devtools")
+    depends_on('py-pandas', when="+devtools")
+    depends_on('py-scikit-learn', when="+devtools")
+    depends_on('py-scipy', when="+devtools")
+    depends_on('xgboost', when="+devtools")
     #depends_on('py-tensorflow') # todo: check if we should integrate.
     #depends_on('py-zfit') # todo: add in spack
+    #depends_on('py-root-pandas') # todo: add in spack
 
 
     ##################### environment boostrap ############
