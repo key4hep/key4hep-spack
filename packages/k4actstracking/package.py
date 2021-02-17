fdc4796f7d011b5c130072b530ef0381a97eae80
Valentin Volkl <valentin.volkl@cern.ch>
2021-02-17 10:47:05 +0100

    add dummy acts package to manage dependencies

---
+++ packages/k4actstracking/package.py
@@ -0,0 +1,17 @@
+
+from spack import *
+from spack.pkg.k4.Ilcsoftpackage import Key4hepPackage
+
+class K4actstracking(BundlePackage, Key4hepPackage):
+    """Acts tracking components for the key4hep project"""
+
+    homepage = "https://github.com/key4hep/k4ActsTracking"
+    #todo
+    #url = "https://github.com/key4hep/k4ActsTracking"
+    git      = "https://github.com/key4hep/k4ActsTracking.git"
+
+    maintainers = ['vvolkl']
+
+    version('main', branch='main')
+
+    depends_on('acts')
