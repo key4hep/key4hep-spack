6d61b0851ceaf9a4fa8a502c8edcc0a0bbd236d0
Valentin Volkl <valentin.volkl@cern.ch>
2021-02-26 09:16:51 +0100

    [fccanalyses] add new version

--- packages/fccanalyses/package.py
+++ packages/fccanalyses/package.py
@@ -10,6 +10,7 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     maintainers = ['vvolkl', 'clementhelsens']
   
     version('master', branch='master')
+    version('0.3.0', sha256='b9ad4f3d9a587f4a1666c9ff5880020f43564a4a0e615d2ce7169bc751134dcf')
     version('0.2.0',      sha256='a4a9965751ae489495f8583129f4f0be4e55e8e676a66a08be35181f2395b955')
     
     depends_on("root")
@@ -17,7 +18,8 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     depends_on("fastjet")
     depends_on('python')
     depends_on('edm4hep')
-    depends_on('fcc-edm')
+    depends_on('fcc-edm', when="@:0.2.9")
+    depends_on('acts', when="@0.3.0:")
 
     # todo: update the cmake config to remove this
     def setup_build_environment(self, spack_env):
@@ -30,3 +32,4 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     def setup_run_environment(self, spack_env):
       spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include.FCCAnalyses)
       spack_env.prepend_path('PYTHONPATH', self.prefix.python)
+      spack_env.set_path("FCCANALYSES", self.prefix.share.FCCAnalyses)
