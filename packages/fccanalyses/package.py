cac8434792ef82fb73a86f9d4c286e87f9db0777
Valentin Volkl <valentin.volkl@cern.ch>
2021-03-01 14:47:58 +0100

    [fccanalyses] new version

--- packages/fccanalyses/package.py
+++ packages/fccanalyses/package.py
@@ -10,6 +10,7 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     maintainers = ['vvolkl', 'clementhelsens']
   
     version('master', branch='master')
+    version('0.3.1', sha256='736e4243493d32744ef5b974ae4e60e43c1ab467ba58df6afdf495fccb165dc3')
     version('0.3.0', sha256='b9ad4f3d9a587f4a1666c9ff5880020f43564a4a0e615d2ce7169bc751134dcf')
     version('0.2.0',      sha256='a4a9965751ae489495f8583129f4f0be4e55e8e676a66a08be35181f2395b955')
     
@@ -19,7 +20,7 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     depends_on('python')
     depends_on('edm4hep')
     depends_on('fcc-edm', when="@:0.2.9")
-    depends_on('acts', when="@0.3.0:")
+    depends_on('acts@5.00.0:', when="@0.3.0:")
     depends_on('eigen', when="@0.3.0:")
 
     # todo: update the cmake config to remove this
@@ -28,6 +29,8 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
       spack_env.prepend_path('CPATH', self.spec['edm4hep'].prefix.include)
       spack_env.prepend_path('CPATH', self.spec['podio'].prefix.include)
       spack_env.prepend_path('CPATH', self.spec['fastjet'].prefix.include)
+      spack_env.prepend_path('CPATH', self.spec['acts'].prefix.include)
+      spack_env.prepend_path('CPATH', self.spec['eigen'].prefix.include)
 
     def setup_run_environment(self, spack_env):
       spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include.FCCAnalyses)
