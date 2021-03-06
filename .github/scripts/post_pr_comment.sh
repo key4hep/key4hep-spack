f2df5679f78462c8f6f8e5a314ac1e6efd8fd9c2
Valentin Volkl <valentin.volkl@cern.ch>
2021-03-06 12:08:45 +0100

    [ci] fixes to concretization

--- .github/scripts/post_pr_comment.sh
+++ .github/scripts/post_pr_comment.sh
@@ -6,7 +6,7 @@ generate_post_data()
 }
 EOF
 }
-PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
+PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 curl -s -H "Authorization: token ${KEY4HEP_COMMENT_BOT_TOKEN}" \
  -X POST -d "$(generate_post_data)"  \
  "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments"
---
+++ scripts/ci_setup_spack2.sh
@@ -0,0 +1,20 @@
+# set up spack inside the k4-spack repo
+ if [ -n "$SPACK_VERSION" ]; then git clone  https://github.com/key4hep/spack ; cd spack; git checkout $SPACK_VERSION; cd ..; else git clone --depth 1 https://github.com/key4hep/spack; fi 
+ source spack/share/spack/setup-env.sh
+# get the right config files to the right places
+ cp config/packages.yaml spack/etc/spack/
+ mkdir spack/var/spack/repos/key4hep-spack
+ cp -r * spack/var/spack/repos/key4hep-spack || true
+# clean up git directories for zip
+ rm -rf spack/var/spack/repos/key4hep-spack/spack || true
+ rm -rf spack/.git
+# register k4 package recipes with spack
+ echo "repos:" > spack/etc/spack/repos.yaml
+ echo ' - $SPACK_ROOT/var/spack/repos/key4hep-spack' >> spack/etc/spack/repos.yaml
+ tar -czf key4hep-spack.tar.gz spack
+ cp ${PWD}/spack/var/spack/repos/key4hep-spack/config/cvmfs_build/upstreams.yaml spack/etc/spack/
+ cp config/compilers_centos7_cvmfs.yaml spack/etc/spack/compilers.yaml
+# compiler setup 
+# spack load gcc
+# spack compiler find --scope site
+# tar -czf key4hep-spack_centos7-cvmfs.tar.gz spack
