3a3de4db42b067615b26da95767a228e20853a9a
Valentin Volkl <valentin.volkl@cern.ch>
2021-03-12 18:18:22 +0100

    [test] adapt to changed LCGEO env var

--- scripts/ci_install_tests.sh
+++ scripts/ci_install_tests.sh
@@ -1,5 +1,5 @@
 geoPluginRun -h
 ddsim -h
 k4run -h
-ddsim --compactFile $lcgeo_DIR/share/lcgeo/compact/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml  -G -N 10 --gun.particle=mu- --gun.distribution uniform --gun.energy "1*GeV" -O muons.slcio 
+ddsim --compactFile $LCGEO/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml  -G -N 10 --gun.particle=mu- --gun.distribution uniform --gun.energy "1*GeV" -O muons.slcio 
 
