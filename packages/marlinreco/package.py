590005889bb3679317ae8bedcb056326ac2fafd6
Thomas Madlener <thomas.madlener@desy.de>
2021-03-29 15:34:54 +0200

    [ilcsoft] Add newest versions

--- packages/marlinreco/package.py
+++ packages/marlinreco/package.py
@@ -19,6 +19,8 @@ class Marlinreco(CMakePackage, Ilcsoftpackage):
     version('master', branch='master')
     k4_add_latest_commit_as_version(git)
     version('1.30', sha256='e3b22a3f974232e4cc785326ad0dfd283b377cffda3245166f419b170276b6ff')
+    version('1.29', sha256='45a36bb98f26580c182848d73ef0423290f99eae380f0cb27eea48f4ef48e459')
+    version('1.28', sha256='f0a6a081d9816502950e639bc209f8f264e63ff3ba555640eff3fb61fb4bdd1d')
     version('1.27', sha256='097462b714e9a47c90154ae1a82de44946d6473b07a659c810263ae53dc8253c')
 
     depends_on('ilcutil')
