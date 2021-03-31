5270f58b0df6e6dec36dcf7c83209ea9f9708aac
Valentin Volkl <valentin.volkl@cern.ch>
2021-03-31 19:37:34 +0200

    k4simgeant4: add new pre-release

--- packages/k4simgeant4/package.py
+++ packages/k4simgeant4/package.py
@@ -11,11 +11,11 @@ class K4simgeant4(CMakePackage, Key4hepPackage):
     maintainers = ['vvolkl']
 
     version('main', branch='main')
-    version("0.1.0pre03", tag="v0.1.0pre03")
+    version("0.1.0pre04", tag="v0.1.0pre04")
 
     variant('cxxstd',
             default='17',
-            values=('14', '17'),
+            values=('14', '17', '20'),
             multi=False,
             description='Use the specified C++ standard when building.')
 
