eb6f8945dafacea511a4aeb4b4f7f13357624262
Valentin Volkl <valentin.volkl@cern.ch>
2021-04-01 16:23:14 +0200

    k4lcioreader: add version 0.3.1 and format recipe (#188)

--- packages/k4lcioreader/package.py
+++ packages/k4lcioreader/package.py
@@ -11,14 +11,14 @@ class K4lcioreader(CMakePackage, Key4hepPackage):
     maintainers = ['mirguest']
 
     version('master', branch='master')
-    k4_add_latest_commit_as_version(git)
-    version('0.1.0', sha256='996d1ff78c0a8a2f7f358dd4ea19f955853ad0902ee86b99c484de58c5fc2e2c')
-    version('0.2.0', sha256='346fc2ba4b4175895597e093f566ba6407be9eeb9cde0766304e0f19ad03e081')
+    version('0.3.1', sha256='cb7614db76972f8b22ccb69cfaa19931b8fdb60f8a506d2b8ff524dd6fcbf0a9')
     version('0.3.0', sha256='ce6a5596a21bfa23e641eda84830d8aa7caf15fd47da35878ee61e7f555d0e51')
+    version('0.2.0', sha256='346fc2ba4b4175895597e093f566ba6407be9eeb9cde0766304e0f19ad03e081')
+    version('0.1.0', sha256='996d1ff78c0a8a2f7f358dd4ea19f955853ad0902ee86b99c484de58c5fc2e2c')
 
     variant('cxxstd',
             default='17',
-            values=('14', '17'),
+            values=('14', '17', '20'),
             multi=False,
             description='Use the specified C++ standard when building.')
 
