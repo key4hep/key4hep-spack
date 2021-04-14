ac60667c61fdcdd9d80fe3db7168aeea3e8a0178
Placido Fernandez Declara <placido.fernandez@cern.ch>
2021-04-14 18:31:03 +0200

    update k4lcioreader and k4marlinwrapper dependency on it

--- packages/k4lcioreader/package.py
+++ packages/k4lcioreader/package.py
@@ -1,6 +1,6 @@
 
 from spack import *
-from spack.pkg.k4.Ilcsoftpackage import Key4hepPackage, k4_add_latest_commit_as_version 
+from spack.pkg.k4.Ilcsoftpackage import Key4hepPackage, k4_add_latest_commit_as_version
 
 class K4lcioreader(CMakePackage, Key4hepPackage):
     """LCIO reader based on PODIO and EDM4hep"""
@@ -11,6 +11,7 @@ class K4lcioreader(CMakePackage, Key4hepPackage):
     maintainers = ['mirguest']
 
     version('master', branch='master')
+    version('0.3.2', sha256='dac76172d484faa23ca170fc89b4cfec6ce56777cd0c20b2b608838824582447')
     version('0.3.1', sha256='cb7614db76972f8b22ccb69cfaa19931b8fdb60f8a506d2b8ff524dd6fcbf0a9')
     version('0.3.0', sha256='ce6a5596a21bfa23e641eda84830d8aa7caf15fd47da35878ee61e7f555d0e51')
     version('0.2.0', sha256='346fc2ba4b4175895597e093f566ba6407be9eeb9cde0766304e0f19ad03e081')
