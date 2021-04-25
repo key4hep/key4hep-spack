b259af186fcd1ffd4dd044b3da6274e68560a8a3
Valentin Volkl <valentin.volkl@cern.ch>
2021-04-25 22:55:25 +0200

    physsim: add patch for TString include

--- packages/physsim/package.py
+++ packages/physsim/package.py
@@ -4,10 +4,8 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-from spack.pkg.k4.Ilcsoftpackage import ilc_url_for_version, k4_add_latest_commit_as_version
-
-
-class Physsim(CMakePackage):
+from spack.pkg.k4.Ilcsoftpackage import Ilcsoftpackage 
+class Physsim(CMakePackage, Ilcsoftpackage):
     """Physsim is a matrix element package."""
 
     url      = "https://github.com/iLCSoft/Physsim/archive/v00-04-01.tar.gz"
@@ -19,13 +17,13 @@ class Physsim(CMakePackage):
     tags = ['hep']
 
     version('master', branch='master')
-    k4_add_latest_commit_as_version(git)
     version('0.4.1', sha256='4c22eee5dcccb764a5ff90850aeb33563c45a14af8939a3ebea736c7d92ac1c1')
 
     depends_on('ilcutil')
     depends_on('root')
 
     patch("dict.patch")
+    patch("TString_include.patch")
 
     def setup_run_environment(self, spack_env):
         spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libPhyssim.so")
@@ -42,5 +40,3 @@ class Physsim(CMakePackage):
         args.append('-DBUILD_TESTING=%s' % self.run_tests)
         return args
 
-    def url_for_version(self, version):
-       return ilc_url_for_version(self, version)
