4165ef0c32a917c5b98d7c4ec657a4cac5449fc8
Valentin Volkl <valentin.volkl@cern.ch>
2021-05-10 09:30:26 +0200

    use environment in scripts

--- scripts/ci_setup_spack.sh
+++ scripts/ci_setup_spack.sh
@@ -3,6 +3,7 @@
  source spack/share/spack/setup-env.sh
 # get the right config files to the right places
  cp environments/key4hep-common/packages.yaml spack/etc/spack/
+ cp environments/key4hep-common/compilers.yaml spack/etc/spack/
  mkdir spack/var/spack/repos/key4hep-spack
  cp -r * spack/var/spack/repos/key4hep-spack || true
 # clean up git directories for zip
@@ -13,7 +14,4 @@
  echo ' - $SPACK_ROOT/var/spack/repos/key4hep-spack' >> spack/etc/spack/repos.yaml
  tar -czf key4hep-spack.tar.gz spack
  cp ${PWD}/spack/var/spack/repos/key4hep-spack/config/cvmfs_build/upstreams.yaml spack/etc/spack/
-# compiler setup 
- spack load gcc
- spack compiler find --scope site
  tar -czf key4hep-spack_centos7-cvmfs.tar.gz spack
