8c33b8147636fca1711bb79854e689dafc49add1
Valentin Volkl <valentin.volkl@cern.ch>
2021-07-08 13:19:47 +0200

    fccanalyses: add py-awkward dependency

--- packages/fccanalyses/package.py
+++ packages/fccanalyses/package.py
@@ -21,6 +21,7 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     depends_on("fastjet")
     depends_on('python')
     depends_on('edm4hep')
+    depends_on('py-awkward')
     depends_on('fcc-edm', when="@:0.2.9")
     depends_on('acts@5.00.0', when="@0.3.0:")
     depends_on('eigen', when="@0.3.0:")
