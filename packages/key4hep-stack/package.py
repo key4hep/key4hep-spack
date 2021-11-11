d5478148f7b078362d750350423abb6113a9a1a9
Valentin Volkl <valentin.volkl@cern.ch>
2021-11-11 17:20:54 +0100

    add k4clue to stack

--- packages/key4hep-stack/package.py
+++ packages/key4hep-stack/package.py
@@ -81,6 +81,9 @@ class Key4hepStack(BundlePackage, Key4hepPackage):
     k4_add_latest_commit_as_dependency("k4simgeant4", "hep-fcc/k4simgeant4", when="@master",
 				 giturl="https://api.github.com/repos/%s/commits/main")
 
+    depends_on("k4clue")
+    k4_add_latest_commit_as_dependency("k4clue", "key4hep/k4clue", when="@master")
+
     depends_on('k4actstracking')
 
     depends_on("guinea-pig")
