6ecab127e94cdb40bea986b8744b7fd93890199a
Valentin Volkl <valentin.volkl@cern.ch>
2021-12-07 17:45:31 +0100

    cleanup: remove config dir, scripts (#311)

--- scripts/ci_install_tests.sh
+++ scripts/ci_install_tests.sh
@@ -1,5 +1,8 @@
+
+# DD4hep
 geoPluginRun -h
 ddsim -h
-k4run -h
 ddsim --compactFile $LCGEO/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml  -G -N 10 --gun.particle=mu- --gun.distribution uniform --gun.energy "1*GeV" -O muons.slcio 
+# K4FWCore
+k4run -h
 
