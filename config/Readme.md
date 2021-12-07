3b5376bf696f1eaacfe802657add5b19fa0804e3
Valentin Volkl <valentin.volkl@cern.ch>
2021-12-07 17:37:48 +0100

    cleanup: remove config dir, scripts

--- config/Readme.md
+++
@@ -1,6 +0,0 @@
-This directory contains config files for spack. `packages.yaml` sets some more optimal variants and the build target microarchitecture, and `cvmfs_build/config.yaml` controls the directory structure for builds intended for cvmfs.
-
-See https://spack.readthedocs.io/en/latest/configuration.html for how to register these config files with spack.
-
-
-Another file `compilers.yaml` is typically needed by spack, but contains paths to the compiler, which may differ on different build machines. The best way to create this one is to set up the desired compiler and run `spack compiler find`
--- scripts/ci_setup_spack.sh
+++ scripts/ci_setup_spack.sh
@@ -1,21 +1,4 @@
 # set up spack inside the k4-spack repo
  if [ -n "$SPACK_VERSION" ]; then git clone  https://github.com/key4hep/spack ; cd spack; git checkout $SPACK_VERSION; cd ..; else git clone --depth 1 https://github.com/key4hep/spack; fi 
  source spack/share/spack/setup-env.sh
- mkdir spack/var/spack/repos/key4hep-spack
- cp -r * spack/var/spack/repos/key4hep-spack || true
-# clean up git directories for zip
- rm -rf spack/var/spack/repos/key4hep-spack/spack || true
- rm -rf spack/.git
-# register k4 package recipes with spack
- echo "repos:" > spack/etc/spack/repos.yaml
- echo ' - $SPACK_ROOT/var/spack/repos/key4hep-spack' >> spack/etc/spack/repos.yaml
-# get the right config files to the right places
- cp ${PWD}/spack/var/spack/repos/key4hep-spack/config/cvmfs_build/upstreams.yaml spack/etc/spack/
- cp ${PWD}/spack/var/spack/repos/key4hep-spack/environments/key4hep-common/packages.yaml spack/etc/spack/
- cp ${PWD}/spack/var/spack/repos/key4hep-spack/environments/key4hep-common/compilers.yaml spack/etc/spack/
- tar -czf key4hep-spack_centos7-cvmfs.tar.gz spack
-# remove the config files again 
- rm spack/etc/spack/upstreams.yaml
- rm spack/etc/spack/compilers.yaml
- rm spack/etc/spack/packages.yaml
- tar -czf key4hep-spack.tar.gz spack
+spack repo add .
