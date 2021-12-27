e5b61ab2d9dca0ee51d0c79d6612dc9618fc12c7
Joseph C Wang <joequant@gmail.com>
2021-12-28 00:10:39 +0800

    remove USE_CXX11 option as it interferes with newer C++ versions
    the USE_CX11 option of ilcutil inserts a -std=c++11 flag which
    interferes with cmake files that includes ilcutil when the app
    uses a newer cxx version.

--- packages/ilcutil/package.py
+++ packages/ilcutil/package.py
@@ -21,6 +21,8 @@ class Ilcutil(CMakePackage, Ilcsoftpackage):
     version('1.6', sha256='09083890721704f39a3e902dc660db5326027cc38446b813233d04ec3233ba2e')
 
     patch("installdoc.patch", when="@:1.6.1")
+    # Remove use_cxx11 patch since this conflicts with cxxstd
+    patch("remove-set-cxx-standard.patch")
 
     variant('cxxstd',
             default='17',
