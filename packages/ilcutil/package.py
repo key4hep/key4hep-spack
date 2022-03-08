7f00f587f520f5349952731026df210ee6db791a
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-08 14:01:50 +0100

    ilcutil: use option instead of patch to disable use_cxx11

--- packages/ilcutil/package.py
+++ packages/ilcutil/package.py
@@ -21,8 +21,6 @@ class Ilcutil(CMakePackage, Ilcsoftpackage):
     version('1.6', sha256='09083890721704f39a3e902dc660db5326027cc38446b813233d04ec3233ba2e')
 
     patch("installdoc.patch", when="@:1.6.1")
-    # Remove use_cxx11 patch since this conflicts with cxxstd
-    patch("remove-set-cxx-standard.patch")
 
     variant('cxxstd',
             default='17',
@@ -38,5 +36,6 @@ class Ilcutil(CMakePackage, Ilcsoftpackage):
         # C++ Standard
         return [
             self.define_from_variant('CMAKE_CXX_STANDARD', 'cxxstd'),
-            self.define_from_variant('INSTALL_DOC', 'doc')
+            self.define_from_variant('INSTALL_DOC', 'doc'),
+            self.define('USE_CXX11', False),
         ]
