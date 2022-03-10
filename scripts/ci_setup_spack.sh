68dde1ab3fea9f6ff32930e6474c3686e123d8d5
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-10 17:34:12 +0100

    [ci] fixes for nightly builds

--- scripts/ci_setup_spack.sh
+++ scripts/ci_setup_spack.sh
@@ -1,4 +1,4 @@
 # set up spack inside the k4-spack repo
  if [ -n "$SPACK_VERSION" ]; then git clone  https://github.com/key4hep/spack ; cd spack; git checkout $SPACK_VERSION; cd ..; else git clone --depth 1 https://github.com/key4hep/spack; fi 
  source spack/share/spack/setup-env.sh
-spack repo add .
+spack repo add . || true
