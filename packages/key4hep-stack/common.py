33a4eb5fcac5d6ebd514dde4244599d50015a4cb
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-22 16:13:27 +0100

    refactor framework test environment setup and use in clicperformance

--- packages/key4hep-stack/common.py
+++ packages/key4hep-stack/common.py
@@ -4,6 +4,7 @@ Common methods for use in Key4hep recipes
 
 from spack import *
 from spack.directives import *
+from spack.user_environment import *
 
 import os
 import platform
@@ -21,7 +22,16 @@ import spack.store
 from spack.package import PackageBase
 
 
-
+def k4_setup_env_for_framework_tests(env):
+    """Setup for tests that need the run environment.
+    """
+    runenv = environment_modifications_for_spec(self.spec)
+    env.extend(runenv)
+    for dspec in self.spec.traverse(root=False, order='post'):
+      dspec.package.setup_run_environment(env)
+      # make sure that ROOT_INCLUDE_PATH is set
+      if (dspec.satisfies('^root')):
+        self.spec['root'].package.setup_dependent_run_environment(env, dspec)
 
 
 def k4_generate_setup_script(env_mod, shell='sh'):
