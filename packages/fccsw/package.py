e263207bc8e491fe42bfa1603f95fa3932fe0f61
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-22 16:22:28 +0100

    fccsw: fix tests

--- packages/fccsw/package.py
+++ packages/fccsw/package.py
@@ -1,5 +1,7 @@
 
 from spack.pkg.k4.key4hep_stack import Key4hepPackage 
+from spack.pkg.k4.key4hep_stack import k4_setup_env_for_framework_tests
+
 
 class Fccsw(CMakePackage, Key4hepPackage):
     """Software framework of the FCC project"""
@@ -19,15 +21,15 @@ class Fccsw(CMakePackage, Key4hepPackage):
             multi=False,
             description='Use the specified C++ standard when building.')
 
-    depends_on("gaudi", type=("test", "run"))
-    depends_on("k4fwcore", type=("test", "run"))
-    depends_on("k4gen", type=("test", "run"))
-    depends_on("k4simdelphes", type=("test", "run"))
-    depends_on("fccdetectors", type=("test", "run"))
-    depends_on("k4simgeant4", type=("test", "run"))
-    depends_on("k4reccalorimeter", type=("test", "run"))
-    depends_on("lcgeo", type=("test", "run"))
-    depends_on("fccanalyses", type=("test", "run"))
+    depends_on("gaudi")
+    depends_on("k4fwcore")
+    depends_on("k4gen")
+    depends_on("k4simdelphes")
+    depends_on("fccdetectors")
+    depends_on("k4simgeant4")
+    depends_on("k4reccalorimeter")
+    depends_on("lcgeo")
+    depends_on("fccanalyses")
 
     def cmake_args(self):
         args = []
@@ -43,6 +45,6 @@ class Fccsw(CMakePackage, Key4hepPackage):
         spack_env.set("FCCSW", self.prefix.share.FCCSW)
         spack_env.prepend_path("PYTHONPATH", self.prefix.python)
 
-    def setup_build_environment(self, spack_env):
-        spack_env.set("FCCSW", self.prefix.share.FCCSW)
-        spack_env.prepend_path("PYTHONPATH", self.prefix.python)
+    def setup_build_environment(self, env):
+        self.setup_run_environment(env)
+        k4_setup_env_for_framework_tests(env)
