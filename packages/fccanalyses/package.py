5574c596e8d4d642267cdffc2b15d1bed9b5d1a2
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-22 16:05:10 +0100

    fccanalyses: update acts, add dd4hep variant

--- packages/fccanalyses/package.py
+++ packages/fccanalyses/package.py
@@ -16,6 +16,8 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     version('0.3.1', sha256='736e4243493d32744ef5b974ae4e60e43c1ab467ba58df6afdf495fccb165dc3')
     version('0.3.0', sha256='b9ad4f3d9a587f4a1666c9ff5880020f43564a4a0e615d2ce7169bc751134dcf')
     version('0.2.0', sha256='a4a9965751ae489495f8583129f4f0be4e55e8e676a66a08be35181f2395b955')
+
+    variant('dd4hep', default=True, description="Build DD4hep-dependent analyzers.")
     
     depends_on("root")
     depends_on("vdt")
@@ -24,8 +26,9 @@ class Fccanalyses(CMakePackage, Key4hepPackage):
     depends_on('edm4hep')
     depends_on('py-awkward')
     depends_on('fcc-edm', when="@:0.2.9")
-    depends_on('acts@5.00.0', when="@0.3.0:")
+    depends_on('acts', when="@0.3.0:")
     depends_on('eigen', when="@0.3.0:")
+    depends_on('dd4hep', when="@0.3.3: +dd4hep")
 
     # todo: update the cmake config to remove this
     def setup_build_environment(self, spack_env):
