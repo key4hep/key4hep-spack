e1f7364195fd6ed49b8c4ad3126e1d710ec933a6
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-24 17:06:11 +0100

    conddbmysql: format

--- packages/conddbmysql/package.py
+++ packages/conddbmysql/package.py
@@ -7,7 +7,7 @@ from spack.pkg.k4.key4hep_stack import Key4hepPackage
 
 
 class Conddbmysql(CMakePackage, Key4hepPackage):
-    """ Linear Collider MySQL Conditions Database """
+    """Linear Collider MySQL Conditions Database."""
 
     homepage = "https://github.com/iLCSoft/conddbmysql"
     git      = "https://github.com/iLCSoft/conddbmysql.git"
@@ -18,17 +18,11 @@ class Conddbmysql(CMakePackage, Key4hepPackage):
     version('master', branch='master')
     version('0-9-7', sha256='7cbf9e06e2b3d131939ac0b66816814738e8c5021449f19921b4071c1979ef5a')
 
-
-
     depends_on("mariadb")
     depends_on("ilcutil")
-
-
 
     def cmake_args(self):
         args = []  
         # todo: add variant
         #args.append(self.define('LCCD_CONDDBMYSQL', False))
         return args
-
-
