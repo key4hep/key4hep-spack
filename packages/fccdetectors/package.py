0479f29196a1694c8ba60f7fba2c0916534fca9a
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-24 17:16:36 +0100

    fccdetectors: add v1.0pre07

--- packages/fccdetectors/package.py
+++ packages/fccdetectors/package.py
@@ -12,6 +12,7 @@ class Fccdetectors(CMakePackage, Key4hepPackage):
     version('main', branch='main')
     # can be removed once the ci is fixed
     version('master', branch='main')
+    version("0.1pre07", tag="v0.1pre07")
     version("0.1pre06", tag="v0.1pre06")
 
     depends_on('dd4hep')
