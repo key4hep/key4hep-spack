c98e8c8bae0d99427b54157effc5a455caefee94
Valentin Volkl <valentin.volkl@cern.ch>
2022-03-24 16:19:56 +0100

    k4fwcore: add v1.0pre14, update podio dependencies

--- packages/k4fwcore/package.py
+++ packages/k4fwcore/package.py
@@ -4,16 +4,16 @@ from spack.pkg.k4.key4hep_stack import k4_setup_env_for_framework_tests
 
 class K4fwcore(CMakePackage, Ilcsoftpackage):
     """Core framework components of the Key4HEP project"""
+
     homepage = "https://github.com/key4hep/k4FWCore"
     url = "https://github.com/key4hep/k4FWCore/archive/v00-01.tar.gz"
     git = "https://github.com/key4hep/k4FWCore.git"
 
     version('master', branch='master')
-    version('1.0pre12', tag="v01-00pre12") 
+    version('1.0pre14', tag="v01-00pre14") 
     version('0.2.0', sha256='7d1a6e7494f08c2b25901cab2138795f21b6c4e84f05c4f8b9a6839787874b72')
     version('0.1.1', sha256='9c4e4b487f7d9c982547c13570345399505e763fb369b76ceadb35c1d52bf6aa')
     version('0.1.0', sha256='aef682649f3fcb1d72de897fbf6ec4ed421c8a4836bb3462c4b0049a709374e4')
-
 
     variant('build_type', default='Release',
             description='CMake build type',
@@ -29,7 +29,9 @@ class K4fwcore(CMakePackage, Ilcsoftpackage):
     depends_on('gaudi@32.2:34.99', when='@:0.2.99')
     depends_on('root@6.08:')
     depends_on('podio@0.10:')
+    depends_on('podio@0.14.1:', when='@1.0pre14:')
     depends_on('edm4hep')
+    depends_on('edm4hep@0.4.1:', when='@1.0pre14:')
 
     def cmake_args(self):
         args = []
