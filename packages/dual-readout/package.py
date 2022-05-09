8eff282f5e342497dd06d41ec59ada076ac21658
Joseph Wang <joequant@gmail.com>
2022-05-09 16:33:39 +0800

    dual-readout: fix clang compile error (#345)
    * fix clang compile error on dual-readout
    
    see https://github.com/HEP-FCC/dual-readout/pull/28
    
    * add patch by url
    
    Co-authored-by: Valentin Volkl <valentin.volkl@cern.ch>

--- packages/dual-readout/package.py
+++ packages/dual-readout/package.py
@@ -21,6 +21,11 @@ class DualReadout(CMakePackage, Key4hepPackage):
     version('0.0.3', sha256='d35e7193c11385505494f11328d54a595b3ff953563bae06b8954c1ef24209b3')
     version('0.0.2', sha256='f76c1febf3d8e29d5287ba03eacbc244f8c615502295f7471579245376da91ad')
 
+    # backport fix for build error with clang
+    patch('https://github.com/HEP-FCC/dual-readout/commit/31c01d2f7867f6c44da63fdc7db69e30c4bb34bb.diff', when='@0.1.0',
+          sha256='0fc3b0e77d52e1dec72a0dae73f15689720b28298deea1992301e8c41c80271c')
+
+
     depends_on('dd4hep')
     depends_on('edm4hep@0.4.1:', when='@0.1.0:')
     depends_on('podio@0.14.1:', when='@0.1.0:')
