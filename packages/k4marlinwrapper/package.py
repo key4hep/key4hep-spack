1d9512ce74f2010f684608c0a453a5dab51f2b7e
Valentin Volkl <valentin.volkl@cern.ch>
2022-05-25 22:49:45 +0200

    one more edm4hep patch for k4marlinwrapper

--- packages/k4marlinwrapper/package.py
+++ packages/k4marlinwrapper/package.py
@@ -24,6 +24,8 @@ class K4marlinwrapper(CMakePackage, Ilcsoftpackage):
     version('0.1',    sha256='d3048178b2f9b721a64ee296019435cbbbce5a65ad956ec733cdb203730db188')
 
 
+    patch('https://github.com/vvolkl/k4MarlinWrapper/commit/baaf2a42db52e694e34f7dfbdf994320738b073b.patch',
+          sha256='https://github.com/vvolkl/k4MarlinWrapper/commit/baaf2a42db52e694e34f7dfbdf994320738b073b.patch', when='@0.4.1:')
     patch('https://github.com/vvolkl/k4MarlinWrapper/commit/16153968a7e801af20f9bdc65d09f9b58403abb3.patch',
           sha256='a60df93183a187e685d7b3021b19e2eb46e31d2e450ed0ba223b69ff938d2a6a', when='@0.4.1:')
 
