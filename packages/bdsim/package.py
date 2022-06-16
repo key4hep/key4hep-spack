1e30d1fa47f2997b57fa6428213a8973035fcf9d
ganis <gerardo.ganis@cern.ch>
2022-06-16 14:24:32 +0200

    Adding package BDSim, default version v1.6.0
    Beam Delivery Simulation (BDSIM) is a C++ program that utilises the Geant4 toolkit
    to simulate both the transport of particles in an accelerator and their interaction
    with the accelerator material. BDSIM is capable of simulating a wide variety of
    accelerator components, magnets and processes, e.g. sychrotron radiation, with Geant4
    geometry dynamically built based on a text input file. Thick lens accelerator tracking
    routines are provided for fast accurate tracking in a vacuum.

---
+++ packages/bdsim/package.py
@@ -0,0 +1,48 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install bdsim
+#
+# You can edit this file again by typing:
+#
+#     spack edit bdsim
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class Bdsim(CMakePackage):
+    """Beam Delivery Simulation (BDSIM) is a C++ program that utilises the Geant4 toolkit
+       to simulate both the transport of particles in an accelerator and their interaction
+       with the accelerator material"""
+
+    homepage = "http://www.pp.rhul.ac.uk/bdsim/manual/index.html"
+    url      = "https://bitbucket.org/jairhul/bdsim/get/v1.6.0.tar.gz"
+    git      = "https://bitbucket.org/jairhul/bdsim/src/master/"
+
+    tags = ['hep']
+
+    maintainers = ['ganis']
+
+    version('develop', branch='develop')
+    version('1.6.0', sha256='e3241d2d097cb4e22249e315c1474da9b3657b9c6893232d9f9e543a5323f717')
+
+    depends_on('cmake')
+    depends_on('geant4@10.7.3')
+    depends_on('root')
+    depends_on('clhep')
+    depends_on('flex')
+    depends_on('bison')
+
