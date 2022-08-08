7424102cc5e6fb9f76ffc1de1666b7fc0c6878c2
Brieuc Francois <brieuc.francois@cern.ch>
2022-08-08 12:08:07 +0200

    Add K4RECCALORIMETER to the test before publishing releases (#412)

--- scripts/ci_install_tests.sh
+++ scripts/ci_install_tests.sh
@@ -5,4 +5,6 @@ ddsim -h
 ddsim --compactFile $LCGEO/CLIC/compact/CLIC_o3_v14/CLIC_o3_v14.xml  -G -N 10 --gun.particle=mu- --gun.distribution uniform --gun.energy "1*GeV" -O muons.slcio 
 # K4FWCore
 k4run -h
+# K4RecCalorimeter
+fccrun $K4RECCALORIMETER/RecFCCeeCalorimeter/tests/options/runCaloSim.py
 
