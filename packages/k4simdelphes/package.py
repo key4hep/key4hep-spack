e6f92ed3246d6ce8370a14277bac55a3b66c3314
Valentin Volkl <valentin.volkl@cern.ch>
2022-12-14 10:09:21 +0100

    k4simdelphes: fix url

--- packages/k4simdelphes/package.py
+++ packages/k4simdelphes/package.py
@@ -16,7 +16,9 @@ class K4simdelphes(CMakePackage, Ilcsoftpackage):
     maintainers = ['vvolkl']
 
     version('main', branch='main')
-    version('00-03-00', sha256='f2dc577738a3819b33b23ee829a1123f283293512a09c62de166d95e55232776')
+    version('00-03-00',
+            url='https://github.com/key4hep/k4SimDelphes/archive/v00-03-00.tar.gz',
+            sha256='f2dc577738a3819b33b23ee829a1123f283293512a09c62de166d95e55232776')
     version('00-02-01', sha256='2a8fc1ce97fcdbafc0af0b1e13df8005695f92bffe8f0029be952ec6a9eeeb76')
     version("00-02", sha256="ffef851a6726b401ac43a7195d76a4d918ea135795eb1b5baff041c7f10ab105")
     version('00-01-09', sha256='4f91742f0be9bdb01f25ab8ee9c6650267f8a1b587762cb4cc10aacd16dc30f3')
