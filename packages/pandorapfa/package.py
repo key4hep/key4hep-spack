c27381cc989a7a9e8637fd5eddb5f64d231df197
Nazar Bartosik <nazar.bartosik@cern.ch>
2023-03-29 17:23:32 +0200

    #1 Added new versions for pandorapfa (#460)

--- packages/pandorapfa/package.py
+++ packages/pandorapfa/package.py
@@ -19,8 +19,16 @@ class Pandorapfa(Package):
 
     maintainers = ['vvolkl']
 
-    version('master', branch='master')
-    version('3.24.0', sha256='a5a9d091e032a3bbdb383eb3bc5609fd3e57367907f3d07e3270f21ef6758074')
+    version('master',  branch='master')
+    version('4.3.1',   sha256='2f4757a6ed2e10d3effc300b330f67ba13c499dbf21ba720b29b50527332fcdb')
+    version('4.3.0',   sha256='a794022c33b3a5afc1272740ac385e0c4ab96a112733012e7dfcbe80b5a3b445')
+    version('4.2.1',   sha256='1d262417748d18e00466ae3f1714ab0d7452e903bd1430773a72c652cf4666e4')
+    version('4.2.0',   sha256='5c1030db6047b2d6cef6b534a98f5293e0f97f8e35e92f254f2a61b4a20f5cee')
+    version('4.0.0',   sha256='80fdb60ac53ebada9d6ed2c6d0cefe79174586ce82e2e3bee7eefb4dbacbfba3')
+    version('3.25.3',  sha256='b390d85ef8081e3fe090862b084298344fc2a6f3c67e29cb4f2cdced7fa25628')
+    version('3.25.2',  sha256='43eb43fd25bcba95ca391aa349058a3946551771e4373862a7b1a328cbfe3f4d')
+    version('3.25.1',  sha256='dc1b4e910d27bc892be72a66696034bf63f9e5f4aa07d8c1799677b1a8261645')
+    version('3.24.0',  sha256='a5a9d091e032a3bbdb383eb3bc5609fd3e57367907f3d07e3270f21ef6758074')
     version('3.21.01', sha256='36dd20235d924b975c167a450943d5f70c9e76d95eea0f55c68b0eead6c99e47')
     version('3.21.00', sha256='c36070916691bd4137a6a21aced2efd730cfe31a17819cf94511351b6edfec8d')
     version('3.20.05', sha256='dae829821dbc4d662818f5593e9899b482878c993c4fdebcd6e7bfd4b6e0a9fe')
@@ -31,8 +39,8 @@ class Pandorapfa(Package):
     version('3.20.00', sha256='510998cb984fdbcb38b46711bef475df44dd04c5d72a083c4d28b1d5757e0539')
     version('3.19.12', sha256='a908a93fbfada1faea605aad49082d5fd8b4c4d387658975313fb1441a15ae55')
     version('3.19.11', sha256='c426324ca0be497619185ee066e62758d071672ce5402350bfe40eff91c9565d')
-    version('3.19.9', sha256='96e68f455989d523343cdd0513019c9cd9486bcc417962e80b6ffcc7daa3b78d')
-    version('3.14.0', sha256='1490f2504bdbd2960cba35fc552b762e3842d77ed5227f84ddabfde546fe6810')
+    version('3.19.9',  sha256='96e68f455989d523343cdd0513019c9cd9486bcc417962e80b6ffcc7daa3b78d')
+    version('3.14.0',  sha256='1490f2504bdbd2960cba35fc552b762e3842d77ed5227f84ddabfde546fe6810')
 
     patch("path.patch")
 
