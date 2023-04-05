1fcee8f570753b4ae9607b4564dc6ab803608f5b
jmcarcell <jmcarcell@users.noreply.github.com>
2023-04-05 08:23:10 +0200

    Add depth 1 when cloning

--- scripts/ci_setup_spack.sh
+++ scripts/ci_setup_spack.sh
@@ -1,3 +1,6 @@
-# set up spack inside the k4-spack repo
- if [ -n "$SPACK_VERSION" ]; then git clone  https://github.com/key4hep/spack ; cd spack; git checkout $SPACK_VERSION; cd ..; else git clone --depth 1 https://github.com/key4hep/spack; fi 
+if [ -n "$SPACK_VERSION" ]; then
+    git clone https://github.com/key4hep/spack -b $SPACK_VERSION --depth 1
+else
+    git clone --depth 1 https://github.com/key4hep/spack
+fi
  source spack/share/spack/setup-env.sh
