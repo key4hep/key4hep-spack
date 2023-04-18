3b79b4a955dc45b8ebcfc67a04d25b708b59936c
tmadlener <thomas.madlener@desy.de>
2023-04-18 12:02:27 +0200

    [black] format raida and update to conform to spack upstream

--- packages/raida/package.py
+++ packages/raida/package.py
@@ -7,25 +7,28 @@ from spack.pkg.k4.key4hep_stack import Ilcsoftpackage
 
 
 class Raida(CMakePackage, Ilcsoftpackage):
-    """ A utility package for the iLCSoft software framework """
+    """A utility package for the iLCSoft software framework"""
 
     homepage = "https://github.com/iLCSoft/raida"
-    git      = "https://github.com/iLCSoft/raida.git"
-    url      = "https://github.com/iLCSoft/raida/archive/v01-06.tar.gz"
+    git = "https://github.com/iLCSoft/raida.git"
+    url = "https://github.com/iLCSoft/raida/archive/v01-06.tar.gz"
 
-    maintainers = ['vvolkl']
+    maintainers("vvolkl")
 
-    version('master', branch='master')
-    version('1.9.0', sha256='53ad3fd7c62e5eba70e6d6099e5ef4d92920399afb7b31dc8008b6ad865a9e85')
+    version("master", branch="master")
+    version(
+        "1.9.0",
+        sha256="53ad3fd7c62e5eba70e6d6099e5ef4d92920399afb7b31dc8008b6ad865a9e85",
+    )
 
-
-    depends_on('ilcutil')
+    depends_on("ilcutil")
     depends_on("root")
 
     def cmake_args(self):
         args = []
         # C++ Standard
-        args.append('-DCMAKE_CXX_STANDARD=%s'
-                    % self.spec['root'].variants['cxxstd'].value)
-        args.append('-DBUILD_TESTING=%s' % self.run_tests)
+        args.append(
+            "-DCMAKE_CXX_STANDARD=%s" % self.spec["root"].variants["cxxstd"].value
+        )
+        args.append("-DBUILD_TESTING=%s" % self.run_tests)
         return args
