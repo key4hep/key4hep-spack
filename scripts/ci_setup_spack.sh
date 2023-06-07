0cf164bfb8606b232c24a0093da3dce243561ee2
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-07 14:41:49 +0200

    Remove unused script

--- scripts/ci_setup_spack.sh
+++
@@ -1,6 +0,0 @@
-if [ -n "$SPACK_VERSION" ]; then
-    git clone https://github.com/key4hep/spack -b $SPACK_VERSION --depth 1
-else
-    git clone --depth 1 https://github.com/key4hep/spack
-fi
- source spack/share/spack/setup-env.sh
