93e6d1151e440b1ee8afa90683eb0f875ed0ff2e
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-27 15:28:37 +0200

    Fix more issues with libs.directories[0]

--- packages/k4geo/package.py
+++ packages/k4geo/package.py
@@ -66,7 +66,8 @@ class K4geo(CMakePackage):
         env.set('K4GEO', self.prefix.share.k4geo.compact)
         env.set('lcgeo_DIR', self.prefix.share.k4geo.compact)
         env.set('k4geo_DIR', self.prefix.share.k4geo.compact)
-        env.prepend_path("LD_LIBRARY_PATH", self.spec['k4geo'].libs.directories[0])
+        env.prepend_path("LD_LIBRARY_PATH", self.spec['k4geo'].prefix.lib)
+        env.prepend_path("LD_LIBRARY_PATH", self.spec['k4geo'].prefix.lib64)
 
     def setup_build_environment(self, env):
         env.set('LCGEO', self.prefix.share.k4geo.compact)
