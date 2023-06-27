b2d5efe58657c5769272bb4f627834d58fc17f82
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-27 15:03:52 +0200

    Fix another issue with libs.directories[0]

--- packages/k4fwcore/package.py
+++ packages/k4fwcore/package.py
@@ -51,7 +51,8 @@ class K4fwcore(CMakePackage, Ilcsoftpackage):
     def setup_run_environment(self, env):
         env.prepend_path('PYTHONPATH', self.prefix.python)
         env.prepend_path("PATH", self.prefix.scripts)
-        env.prepend_path("LD_LIBRARY_PATH", self.spec['k4fwcore'].libs.directories[0])
+        env.prepend_path("LD_LIBRARY_PATH", self.spec['k4fwcore'].prefix.lib)
+        env.prepend_path("LD_LIBRARY_PATH", self.spec['k4fwcore'].prefix.lib64)
 	
     def setup_build_environment(self, env):
         env.prepend_path('LD_LIBRARY_PATH', self.spec['gaudi'].prefix.lib)
