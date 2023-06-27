3a62594a5107a89479832d77dfe60bc5920cf588
jmcarcell <jmcarcell@users.noreply.github.com>
2023-06-27 22:07:43 +0200

    Fix another issue with libs.directories[0]

--- packages/fccdetectors/package.py
+++ packages/fccdetectors/package.py
@@ -34,7 +34,8 @@ class Fccdetectors(CMakePackage, Key4hepPackage):
         env.prepend_path("LD_LIBRARY_PATH", self.spec['k4geo'].prefix.lib)
         env.prepend_path("LD_LIBRARY_PATH", self.spec['k4geo'].prefix.lib64)
         env.prepend_path("LD_LIBRARY_PATH", self.spec['lcio'].libs.directories[0])
-        env.prepend_path("LD_LIBRARY_PATH", self.spec['fccdetectors'].libs.directories[0])
+        env.prepend_path("LD_LIBRARY_PATH", self.spec['fccdetectors'].prefix.lib)
+        env.prepend_path("LD_LIBRARY_PATH", self.spec['fccdetectors'].prefix.lib64)
 
     def setup_build_environment(self, env):
         env.prepend_path("LD_LIBRARY_PATH", self.spec['k4geo'].prefix.lib)
