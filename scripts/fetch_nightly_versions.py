c285a2a954ba88c629aa50164d1bf4d53b484cdc
tmadlener <thomas.madlener@desy.de>
2023-06-28 13:46:59 +0200

    [format] Format all utility scripts with black

--- scripts/fetch_nightly_versions.py
+++ scripts/fetch_nightly_versions.py
@@ -1,11 +1,12 @@
 import os
 
+
 def k4_lookup_latest_commit(repoinfo, giturl):
     """Use a github-like api to fetch the commit hash of the master branch.
     Constructs and runs a command of the form:
     # curl -s -u user:usertoken https://api.github.com/repos/hep-fcc/fccsw/commits/master -H "Accept: application/vnd.github.VERSION.sha"
     The authentication is optional, but note that the api might be rate-limited quite strictly for unauthenticated access.
-    The envrionment variables 
+    The envrionment variables
       GITHUB_USER
       GITHUB_TOKEN
     can be used for authentication.
@@ -17,25 +18,28 @@ def k4_lookup_latest_commit(repoinfo, giturl):
        p.ex.: "https://api.github.com/repos/%s/commits/master"
     :return: The commit sha of the latest commit for the repo.
     :rtype: str
-      
+
     """
     curl_command = ["curl -s "]
     github_user = os.environ.get("GITHUB_USER", "")
     github_token = os.environ.get("GITHUB_TOKEN", "")
     if github_user and github_token:
-      curl_command += [" -u %s:%s " % (github_user, github_token)]
+        curl_command += [" -u %s:%s " % (github_user, github_token)]
     final_giturl = giturl % repoinfo
     curl_command += [final_giturl]
     # request only the necessary information (commit hash)
     curl_command += [' -H "Accept: application/vnd.github.VERSION.sha" ']
-    curl_command = ' '.join(curl_command)
+    curl_command = " ".join(curl_command)
     commit = os.popen(curl_command).read()
     # check that what we got looks like a hash
     test = int(commit, 16)
     return commit
 
-def k4_add_latest_commit(name, repoinfo, giturl="https://api.github.com/repos/%s/commits/master", variants=""):
-    """ Helper function for adding a package versioned at the latest commit to a spack environment.
+
+def k4_add_latest_commit(
+    name, repoinfo, giturl="https://api.github.com/repos/%s/commits/master", variants=""
+):
+    """Helper function for adding a package versioned at the latest commit to a spack environment.
 
     :param name: spack name of the package, p.ex: "edm4hep"
     :type name: str
@@ -52,7 +56,7 @@ def k4_add_latest_commit(name, repoinfo, giturl="https://api.github.com/repos/%s
     github_user = os.environ.get("GITHUB_USER", "")
     github_token = os.environ.get("GITHUB_TOKEN", "")
     commit = k4_lookup_latest_commit(repoinfo, giturl)
-    print('  - %s@%s=develop' % (name, commit))
+    print("  - %s@%s=develop" % (name, commit))
 
 
 if __name__ == "__main__":
@@ -63,14 +67,24 @@ if __name__ == "__main__":
     k4_add_latest_commit("dd4hep", "aidasoft/dd4hep")
     k4_add_latest_commit("k4fwcore", "key4hep/k4fwcore")
     k4_add_latest_commit("k4projecttemplate", "key4hep/k4-project-template")
-    k4_add_latest_commit("k4simdelphes", "key4hep/k4SimDelphes",
-                         giturl="https://api.github.com/repos/%s/commits/main")
-    k4_add_latest_commit("k4clue", "key4hep/k4clue",
-                         giturl="https://api.github.com/repos/%s/commits/main")
-    k4_add_latest_commit("k4gen", "hep-fcc/k4Gen",
-                         giturl="https://api.github.com/repos/%s/commits/main")
-    k4_add_latest_commit("k4simgeant4", "hep-fcc/k4simgeant4",
-                         giturl="https://api.github.com/repos/%s/commits/main")
+    k4_add_latest_commit(
+        "k4simdelphes",
+        "key4hep/k4SimDelphes",
+        giturl="https://api.github.com/repos/%s/commits/main",
+    )
+    k4_add_latest_commit(
+        "k4clue",
+        "key4hep/k4clue",
+        giturl="https://api.github.com/repos/%s/commits/main",
+    )
+    k4_add_latest_commit(
+        "k4gen", "hep-fcc/k4Gen", giturl="https://api.github.com/repos/%s/commits/main"
+    )
+    k4_add_latest_commit(
+        "k4simgeant4",
+        "hep-fcc/k4simgeant4",
+        giturl="https://api.github.com/repos/%s/commits/main",
+    )
     k4_add_latest_commit("delphes", "delphes/delphes")
     k4_add_latest_commit("fccsw", "hep-fcc/fccsw")
     # todo: figure out the api for the cern gitlab instance
@@ -79,10 +93,16 @@ if __name__ == "__main__":
     # depends_on('whizard@master +lcio +openloops hepmc=2')
     k4_add_latest_commit("dual-readout", "hep-fcc/dual-readout")
     k4_add_latest_commit("fccanalyses", "hep-fcc/fccanalyses")
-    k4_add_latest_commit("fccdetectors", "hep-fcc/fccdetectors",
-                         giturl="https://api.github.com/repos/%s/commits/main")
-    k4_add_latest_commit("k4reccalorimeter", "hep-fcc/k4reccalorimeter",
-                         giturl="https://api.github.com/repos/%s/commits/main")
+    k4_add_latest_commit(
+        "fccdetectors",
+        "hep-fcc/fccdetectors",
+        giturl="https://api.github.com/repos/%s/commits/main",
+    )
+    k4_add_latest_commit(
+        "k4reccalorimeter",
+        "hep-fcc/k4reccalorimeter",
+        giturl="https://api.github.com/repos/%s/commits/main",
+    )
     k4_add_latest_commit("cepcsw", "cepc/cepcsw")
     k4_add_latest_commit("k4lcioreader", "key4hep/k4LCIOReader")
     k4_add_latest_commit("aidatt", "aidasoft/aidatt")
@@ -94,7 +114,7 @@ if __name__ == "__main__":
     k4_add_latest_commit("ddmarlinpandora", "ilcsoft/ddmarlinpandora")
     k4_add_latest_commit("fcalclusterer", "fcalsw/fcalclusterer")
     k4_add_latest_commit("forwardtracking", "ilcsoft/forwardtracking")
-    k4_add_latest_commit('k4edm4hep2lcioconv', 'key4hep/k4edm4hep2lcioconv')
+    k4_add_latest_commit("k4edm4hep2lcioconv", "key4hep/k4edm4hep2lcioconv")
     k4_add_latest_commit("k4marlinwrapper", "key4hep/k4marlinwrapper")
     k4_add_latest_commit("gear", "ilcsoft/gear")
     k4_add_latest_commit("ilcutil", "ilcsoft/ilcutil")
@@ -113,7 +133,7 @@ if __name__ == "__main__":
     k4_add_latest_commit("marlinreco", "ilcsoft/marlinreco")
     k4_add_latest_commit("marlinfastjet", "ilcsoft/marlinfastjet")
     k4_add_latest_commit("marlinkinfit", "ilcsoft/marlinkinfit")
-    k4_add_latest_commit('marlinkinfitprocessors', 'ilcsoft/marlinkinfitprocessors')
+    k4_add_latest_commit("marlinkinfitprocessors", "ilcsoft/marlinkinfitprocessors")
     k4_add_latest_commit("marlintrkprocessors", "ilcsoft/marlintrkprocessors")
     k4_add_latest_commit("marlintrk", "ilcsoft/marlintrk")
     k4_add_latest_commit("overlay", "ilcsoft/overlay")
--- scripts/get_install_times.py
+++ scripts/get_install_times.py
@@ -3,23 +3,25 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('path', help='path to the packages')
+parser.add_argument("path", help="path to the packages")
 
 
 def get_install_times(path):
     packages = os.listdir(path)
     for p in packages:
-        if p.startswith('.'):
+        if p.startswith("."):
             continue
         hash_folder = os.listdir(os.path.join(path, p))[0]
         try:
-            with open(os.path.join(path, p, hash_folder, '.spack', 'install_times.json'), 'r') as f:
+            with open(
+                os.path.join(path, p, hash_folder, ".spack", "install_times.json"), "r"
+            ) as f:
                 data = json.load(f)
-                print(p, data['total']['seconds'])
+                print(p, data["total"]["seconds"])
         except FileNotFoundError:
-            print(f'{p}: spack install time not found')
+            print(f"{p}: spack install time not found")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
     get_install_times(args.path)
