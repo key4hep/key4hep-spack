60b6bd077aee9a3237370a06cc00c218277f9eb1
jmcarcell <jmcarcell@users.noreply.github.com>
2023-11-23 09:32:55 +0100

    Add a couple of versions

--- packages/k4clue/package.py
+++ packages/k4clue/package.py
@@ -20,6 +20,10 @@ class K4clue(CMakePackage, Ilcsoftpackage):
     version("main", branch="main")
 
     version(
+        "1.0.4",
+        sha256="2b18476ced196f64c2527ed8c68c660e87b4aefbb952de91d2658eb1e352a68a",
+    )
+    version(
         "1.0.1",
         sha256="bf6c1c626bd21684bca8313525e7f6520bde2b7cd666ecce935209c741d93aec",
     )
