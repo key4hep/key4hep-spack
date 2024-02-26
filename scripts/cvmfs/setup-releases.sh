0c4f5e5e1ab26a08fa8a9a6515126dbecd8fe37b
Brieuc Francois <brieuc.francois@cern.ch>
2024-02-26 11:09:21 +0100

    Fix setup script (#564)

--- scripts/cvmfs/setup-releases.sh
+++ scripts/cvmfs/setup-releases.sh
@@ -188,7 +188,7 @@ k4_local_repo() {
     export CMAKE_PREFIX_PATH=$PWD/$install:$CMAKE_PREFIX_PATH
     export PKG_CONFIG_PATH=$PWD/$install/lib/pkgconfig:$PKG_CONFIG_PATH
     export ROOT_INCLUDE_PATH=$PWD/$install/include:$ROOT_INCLUDE_PATH
-    if [ "$current_repo" "k4geo" ]; then
+    if [ "$current_repo" = "k4geo" ]; then
         export K4GEO=$PWD
         echo "Added K4GEO=$PWD"
     fi
