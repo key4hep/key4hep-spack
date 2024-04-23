d747c049627e96baa20ca0d225eedba164f90981
jmcarcell <jmcarcell@users.noreply.github.com>
2024-04-23 21:37:28 +0200

    Fix recipe

--- packages/k4reco/package.py
+++ packages/k4reco/package.py
@@ -1,7 +1,7 @@
-from spack.pkg.k4.key4hep_stack import Ilcsoftpackage
+from spack.pkg.k4.key4hep_stack import Key4hepPackage
 from spack.pkg.k4.key4hep_stack import k4_setup_env_for_framework_tests
 
-class K4reco(CMakePackage, Ilcsoftpackage):
+class K4reco(CMakePackage, Key4hepPackage):
     """Reconstruction algorithms using Gaudi in native key4hep"""
 
     homepage = "https://github.com/key4hep/k4Reco"
@@ -15,7 +15,6 @@ class K4reco(CMakePackage, Ilcsoftpackage):
     depends_on("k4fwcore")
     depends_on("gaudi")
     depends_on("dd4hep")
-    depends_on("lcio")
     depends_on("k4simgeant4")
 
     def cmake_args(self):
@@ -38,8 +37,3 @@ class K4reco(CMakePackage, Ilcsoftpackage):
         env.prepend_path("PYTHONPATH", self.prefix.python)
         env.prepend_path("LD_LIBRARY_PATH", self.spec["k4reco"].prefix.lib)
         env.prepend_path("LD_LIBRARY_PATH", self.spec["k4reco"].prefix.lib64)
-
-    def setup_build_environment(self, env):
-        env.prepend_path("LD_LIBRARY_PATH", self.spec["gaudi"].prefix.lib)
-        env.prepend_path("LD_LIBRARY_PATH", self.spec["gaudi"].prefix.lib64)
-        # k4_setup_env_for_framework_tests(self.spec, env)
