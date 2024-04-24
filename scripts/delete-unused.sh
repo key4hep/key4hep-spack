9bd45cdddc2094c6339336d9cfa48aa7c5838746
jmcarcell <jmcarcell@users.noreply.github.com>
2024-04-24 16:13:26 +0200

    Add a command to print the packages that will be deleted

--- scripts/delete-unused.sh
+++ scripts/delete-unused.sh
@@ -1,3 +1,6 @@
 # This will delete unused (from example from a previous failed installation) specs that
 # are no longer in the current installation
 spack -e . python -c 'from spack.cmd import uninstall; uninstall.do_uninstall([x for x in spack.store.STORE.db.query_local() if not any(x.satisfies(y) for y in spack.environment.active_environment().all_specs_generator())], False)'
+
+# Same command but only print what would be deleted
+spack -e . python -c 'print([x for x in spack.store.STORE.db.query_local() if not any(x.satisfies(y) for y in spack.environment.active_environment().all_specs_generator())])'
