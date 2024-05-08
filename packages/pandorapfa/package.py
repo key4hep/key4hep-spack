ab1ffd9802eea9c50c37f9c8966ea7c22e415691
jmcarcell <jmcarcell@users.noreply.github.com>
2024-05-08 16:35:51 +0200

    Clean up pandorapfa

--- packages/pandorapfa/package.py
+++ packages/pandorapfa/package.py
@@ -19,6 +19,10 @@ class Pandorapfa(Package):
 
     version("master", branch="master")
     version(
+        "4.8.1",
+        sha256sum="3b9e224196f09f3ce59062e73e1d05d4e8821d8163025263eee4448f7529f780",
+    )
+    version(
         "4.4.1",
         sha256="98c628b5063695fa73649136ded0aaa8e40cb34d06000096117606d926599f3b",
     )
@@ -41,30 +45,6 @@ class Pandorapfa(Package):
     version(
         "4.0.0",
         sha256="80fdb60ac53ebada9d6ed2c6d0cefe79174586ce82e2e3bee7eefb4dbacbfba3",
-    )
-    version(
-        "3.25.3",
-        sha256="b390d85ef8081e3fe090862b084298344fc2a6f3c67e29cb4f2cdced7fa25628",
-    )
-    version(
-        "3.25.2",
-        sha256="43eb43fd25bcba95ca391aa349058a3946551771e4373862a7b1a328cbfe3f4d",
-    )
-    version(
-        "3.25.1",
-        sha256="dc1b4e910d27bc892be72a66696034bf63f9e5f4aa07d8c1799677b1a8261645",
-    )
-    version(
-        "3.24.0",
-        sha256="a5a9d091e032a3bbdb383eb3bc5609fd3e57367907f3d07e3270f21ef6758074",
-    )
-    version(
-        "3.21.01",
-        sha256="36dd20235d924b975c167a450943d5f70c9e76d95eea0f55c68b0eead6c99e47",
-    )
-    version(
-        "3.21.00",
-        sha256="c36070916691bd4137a6a21aced2efd730cfe31a17819cf94511351b6edfec8d",
     )
 
     patch("path.patch")
