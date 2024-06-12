3f50e2c3b95dd3e628256f198535d2a00b3bf14a
tmadlener <thomas.madlener@desy.de>
2024-06-12 16:37:40 +0200

    Add MarlinMLFlavorTagging to ilcsoft stack

--- packages/marlinmlflavortagging/package.py
+++ packages/marlinmlflavortagging/package.py
@@ -22,8 +22,16 @@ class Marlinmlflavortagging(CMakePackage, Ilcsoftpackage):
     depends_on("root")
     depends_on("py-torch")
 
+    @property
+    def libs(self):
+        # Make the library that should go on MARLIN_DLL available via this
+        # property by explicitly finding it (also takes care of lib vs lib64)
+        return find_libraries(
+            "libMarlinMLFlavorTagging", root=self.prefix, recursive=True, shared=True
+        )
+
     def cmake_args(self):
         return [f"-DCMAKE_CXX_STANDARD={self.spec['root'].variants['cxxstd'].value}"]
 
     def setup_run_environment(self, env):
-        env.prepend_path("MARLIN_DLL", self.libs.directories[0])
+        env.prepend_path("MARLIN_DLL", self.libs.libraries[0])
