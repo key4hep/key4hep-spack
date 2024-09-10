0fbc7d4acc72f62b1fdbab812d84ebb2dd15d5d1
Joseph C Wang <joequant@gmail.com>
2024-09-10 08:59:41 +0800

    add header for getopt for gcc14 build

--- packages/k4generatorsconfig/package.py
+++ packages/k4generatorsconfig/package.py
@@ -24,6 +24,11 @@ class K4generatorsconfig(CMakePackage):
     depends_on("heppdt")
     depends_on("pythia8")
 
+    patch(
+        "https://github.com/key4hep/k4GeneratorsConfig/commit/29f438dbc0955cf63c682c1bb2a27cd27273bc70.patch?full-index=1",
+        sha256="4a970512113e4e474cd8a8f7db6c47f8decda500a7499088facc7adff17fc4e2"
+    )
+
     def cmake_args(self):
         args = []
         args.append(
