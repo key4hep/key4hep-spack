c832c87e539eba4cd5eae35963593e9504b3ba20
Joseph C Wang <joequant@gmail.com>
2024-09-14 19:06:38 +0800

    fccsw: add include for py-onnxruntime

--- packages/fccsw/package.py
+++ packages/fccsw/package.py
@@ -28,6 +28,7 @@ class Fccsw(CMakePackage, Key4hepPackage):
     depends_on("k4geo")
     depends_on("fccanalyses")
     depends_on("root")
+    depends_on("py-onnxruntime")
     depends_on("py-six", type=("build", "run"))
 
     def cmake_args(self):
