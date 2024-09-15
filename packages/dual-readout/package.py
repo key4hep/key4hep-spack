776e3aa0588a13d1177a1ed151f65275aa7d0468
jmcarcell <jmcarcell@users.noreply.github.com>
2024-09-15 18:38:15 +0200

    Add a patch for dual-readout

--- packages/dual-readout/package.py
+++ packages/dual-readout/package.py
@@ -29,22 +29,17 @@ class DualReadout(CMakePackage, Key4hepPackage):
         "0.1.1",
         sha256="8d856b47b0b834ac0a53920434da210639c55a1ef375f7e0341731ad14a25318",
     )
-    version(
-        "0.1.0",
-        sha256="f4b9387ccae0d4d364b1340eb116c5b4b93a6bc74c896fcd221619ddec31d5f6",
+
+    patch(
+        "https://patch-diff.githubusercontent.com/raw/HEP-FCC/dual-readout/pull/40.patch?full_index=1",
+        sha256="9ff1cad595e631336f49c2430e147f29ddedb5e3eee650c36f9147f420f62423",
+        when="@0.1.3: %gcc@14",
     )
 
-    # backport fix for build error with clang
-    patch(
-        "https://github.com/HEP-FCC/dual-readout/commit/31c01d2f7867f6c44da63fdc7db69e30c4bb34bb.diff",
-        when="@0.1.0",
-        sha256="0fc3b0e77d52e1dec72a0dae73f15689720b28298deea1992301e8c41c80271c",
-    )
 
     depends_on("dd4hep")
     depends_on("edm4hep")
-    depends_on("podio@0.14.1:")
-    depends_on("podio@0.15:", when="@0.1.1:")
+    depends_on("podio")
     depends_on("fastjet")
     depends_on("root")
     depends_on("pythia8")
@@ -63,7 +58,6 @@ class DualReadout(CMakePackage, Key4hepPackage):
 
     def cmake_args(self):
         args = []
-        # C++ Standard
         args.append(
             f"-DCMAKE_CXX_STANDARD={self.spec['root'].variants['cxxstd'].value}"
         )
