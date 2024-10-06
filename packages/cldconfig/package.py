9902c499c266a2ccf6cccb1baa9b618d74f5a635
jmcarcell <jmcarcell@users.noreply.github.com>
2024-10-06 22:08:40 +0200

    Add a version for CLDConfig

--- packages/cldconfig/package.py
+++ packages/cldconfig/package.py
@@ -11,13 +11,13 @@ class Cldconfig(CMakePackage):
 
     homepage = "https://github.com/key4hep/CLDConfig"
     git = "https://github.com/key4hep/CLDConfig"
+    url = "https://github.com/key4hep/CLDConfig/archive/refs/tags/r2024-10-06.tar.gz"
 
     maintainers = ["jmcarcell"]
 
     version("main", branch="main")
-    # Old tag to make sure there is at least one, can be removed when
-    # there is another
-    version("r2024-04-12", tag="r2024-04-12")
+    version("2024-10-06", sha256="7ded5bc3f63eed6b6806d9581e1b47d7ca14a0c97ef5d61331d4ef88b1d7d643")
+    version("2024-04-12", sha256="8a15971152391da3b41bbf543316ad6b41949cd3d3c82e334a7a7b86092591da")
 
     depends_on("k4geo", type="test")
     depends_on("dd4hep", type="test")
