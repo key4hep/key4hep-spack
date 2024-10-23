b9990f32ad63e5af3bda9f7f9d5492b272db55e8
jmcarcell <jmcarcell@users.noreply.github.com>
2024-10-23 16:55:02 +0200

    Add the py-pytransport package

---
+++ packages/py-pytransport/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+class PyPytransport(PythonPackage):
+    """A Python based converter for TRANSPORT files to BDSIM readable gmad files"""
+
+    homepage = "https://bitbucket.org/jairhul/pytransport"
+    url = "https://bitbucket.org/jairhul/pytransport"
+    git = "https://bitbucket.org/jairhul/pytransport.git"
+
+    tags = ["hep"]
+
+    maintainers = ["jmcarcell"]
+
+    version('master', branch='master')
+
+    depends_on("py-matplotlib@3:")
+    depends_on("py-numpy@1.14:")
+    depends_on("py-importlib-metadata")
+    depends_on("py-scipy")
