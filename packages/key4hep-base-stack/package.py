1f56e54efb968234ae6f766c2180a384fc03a4cf
Thomas Madlener <thomas.madlener@desy.de>
2024-11-20 11:52:00 +0100

    Add a base package that bundles a bunch of common deps

---
+++ packages/key4hep-base-stack/package.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+from spack.pkg.k4.key4hep_stack import Key4hepPackage, install_setup_script
+
+
+class Key4hepBaseStack(BundlePackage, Key4hepPackage):
+    """Bundle package that contains a base software stack upon which Key4hep can
+    be built
+
+    The packages in this base stack are mainly
+    """
+
+    homepage = "https://cern.ch/key4hep"
+
+    version(datetime.today().strftime("%Y-%m-%d"))
+
+    # this bundle package installs a custom setup script, so
+    # need to add the install phase (which normally does not
+    # exist for a bundle package)
+    phases = ["install"]
+
+    # Some generally useful development tools
+    depends_on("cmake")
+    depends_on("ninja")
+    depends_on("python")
+    depends_on("gdb")
+    depends_on("catch2@3:")
+    depends_on("boost")
+    depends_on("py-pytest")
+
+    # general hep packages
+    depends_on("root")
+    depends_on("geant4")
+    depends_on("pythia8")
+    depends_on("hepmc3")
+    depends_on("evtgen +photos+tauola+pythia8+hepmc3")
+    depends_on("heppdt")
+    depends_on("fastjet")
+
+    # podio dependencies
+    depends_on("py-pyyaml")
+    depends_on("py-tabulate")
+    depends_on("py-jinja2")
+    depends_on("py-graphviz")
+
+    # other general deps
+    depends_on("py-numpy")
+    depends_on("py-scipy")
+
+    # gaudi dependencies
+    depends_on("py-nose")
+    depends_on("cppgsl")
+    depends_on("fmt")
+    depends_on("cppunit")
+    depends_on("gperftools")
+    depends_on("py-networkx")
+    depends_on("py-six")
+    depends_on("range-v3")
+    depends_on("py-pytest-cov")
+    depends_on("jemalloc")
+    depends_on("aida")
+
+    def install(self, spec, prefix):
+        return install_setup_script(self, spec, prefix, "K4_BASE_STACK")
