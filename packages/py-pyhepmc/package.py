413b9d6e236cdb3d9eda24dd0088fcc1b565ac39
jmcarcell <jmcarcell@users.noreply.github.com>
2024-11-20 09:06:28 +0100

    Fix the package

--- packages/py-pyhepmc/package.py
+++ packages/py-pyhepmc/package.py
@@ -8,7 +8,7 @@ class PyPyhepmc(PythonPackage):
     """Python bindings for HepMC3"""
 
     homepage = "https://github.com/scikit-hep/pyhepmc"
-    url = "https://github.com/scikit-hep/pyhepmc/archive/refs/tags/v2.14.0.tar.gz"
+    pypi = "pyhepmc/pyhepmc-2.14.0.tar.gz"
     git = "https://github.com/scikit-hep/pyhepmc.git"
 
     tags = ["hep"]
@@ -19,12 +19,17 @@ class PyPyhepmc(PythonPackage):
 
     version(
         "2.14.0",
-        sha256="cb20976b017edd7456492134243faf5d3091c6b9fa52bcc5c3ac6e7c6912fe52",
+        sha256="17a6f941e4fa06d08a628990f6816d1da5e545d65f533e6f598740d2cb76ace4",
     )
 
     depends_on("cmake", type="build")
-    depends_on("py-setuptools", type="build")
-    depends_on("py-importlib-resources", type=("build", "run"))
+    depends_on("py-setuptools-scm", type="build")
 
-    depends_on("hepmc3")
-    depends_on("py-numpy@1.21:")
+    depends_on("hepmc3", type=("build", "run"))
+    depends_on("py-numpy@1.21:", type=("build", "run"))
+    depends_on("py-pybind11", type=("build", "run"))
+    depends_on("graphviz", type="run")
+
+    @run_before("install")
+    def use_external_pybind11(self):
+        filter_file("(EXTERNAL_PYBIND11.*) OFF", r"\1 ON", "CMakeLists.txt")
