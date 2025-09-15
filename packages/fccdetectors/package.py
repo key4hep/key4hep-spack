80dc04e5604474f22e4d16b9b76dbd10e682b67c
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2025-09-15 08:29:36 +0200

    Add a new tag for FCCDetectors

--- packages/fccdetectors/package.py
+++ packages/fccdetectors/package.py
@@ -14,6 +14,7 @@ class Fccdetectors(CMakePackage, Key4hepPackage):
     version("main", branch="main")
     # can be removed once the ci is fixed
     version("master", branch="main")
+    version("0.1pre11", tag="v0.1pre11")
     version("0.1pre10", tag="v0.1pre10")
     version("0.1pre09", tag="v0.1pre09")
     version("0.1pre08", tag="v0.1pre08")
