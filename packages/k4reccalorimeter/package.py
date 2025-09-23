8043e0590674e169609bef2935606bfdd98aead2
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2025-09-23 18:43:16 +0200

    Fix syntax

--- packages/k4reccalorimeter/package.py
+++ packages/k4reccalorimeter/package.py
@@ -51,11 +51,10 @@ class K4reccalorimeter(CMakePackage, Key4hepPackage):
     depends_on("simsipm")
 
     def cmake_args(self):
-        args = []
-        args.extend(
+        args = [
             f"-DCMAKE_CXX_STANDARD={self.spec['root'].variants['cxxstd'].value}",
             "-DCMAKE_INSTALL_LIBDIR=lib",
-        )
+        ]
         return args
 
     def setup_run_environment(self, env):
