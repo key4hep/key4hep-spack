b9437f311b00c1c6a02628ed943a10acd496a314
Thomas Madlener <thomas.madlener@desy.de>
2025-10-23 16:38:04 +0200

    k4actstracking: Add first tag

--- packages/k4actstracking/package.py
+++ packages/k4actstracking/package.py
@@ -7,14 +7,17 @@ class K4actstracking(CMakePackage, Key4hepPackage):
 
     homepage = "https://github.com/key4hep/k4ActsTracking"
     # todo
-    # url = "https://github.com/key4hep/k4ActsTracking"
+    url = "https://github.com/key4hep/k4ActsTracking/archive/refs/tags/v00-01.tar.gz"
     git = "https://github.com/key4hep/k4ActsTracking.git"
 
     maintainers("vvolkl")
 
     version("main", branch="main")
+    version(
+        "00-01",
+        sha256="05901153900064673417b559d4a4a5d9ebc6e783367130a5b20abe1c83bc39eb",
+    )
 
-    depends_on("c", type="build")
     depends_on("cxx", type="build")
 
     depends_on("acts+dd4hep+tgeo+json")
