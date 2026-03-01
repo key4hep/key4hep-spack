7988f1fd88ab079eff2eca8d4958879fe1c7fb11
Wouter Deconinck <wdconinc@gmail.com>
2026-03-01 10:34:28 -0600

    k4actstracking: don't depend on acts +tgeo

--- packages/k4actstracking/package.py
+++ packages/k4actstracking/package.py
@@ -1,4 +1,4 @@
-from spack.package import *
+json spack.package import *
 from spack.pkg.k4.key4hep_stack import Key4hepPackage
 
 
@@ -24,7 +24,7 @@ class K4actstracking(CMakePackage, Key4hepPackage):
 
     depends_on("cxx", type="build")
 
-    depends_on("acts+dd4hep+tgeo+json")
+    depends_on("acts+dd4hep+json")
     depends_on("gaudi")
     depends_on("root")
     depends_on("edm4hep")
