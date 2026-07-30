e9817c2f14a1b132a85f3cd10d7c36f7bdf70768
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2026-07-30 21:44:43 +0200

    Fix setup-nightlies.sh compatibility with 'set -u' (#902)

--- scripts/cvmfs/setup-nightlies.sh
+++ scripts/cvmfs/setup-nightlies.sh
@@ -192,7 +192,7 @@ lcg_setup=0
 
 for ((i=1; i<=$#; i++)); do
     eval arg=\$$i
-    eval "argn=\${$((i+1))}"
+    eval "argn=\${$((i+1)):-}"
     case $arg in
         --lcg)
             lcg_setup=1
@@ -247,7 +247,7 @@ if [ $lcg_setup -eq 1 ]; then
         return 1
     fi
 
-    if [ -n "$KEY4HEP_STACK" ]; then
+    if [ -n "${KEY4HEP_STACK:-}" ]; then
         echo "The Key4hep software stack is already set up, please start a new shell to avoid conflicts"
         return 1
     fi
@@ -275,18 +275,18 @@ if [ $lcg_setup -eq 1 ]; then
 fi
 
 rel="latest-$build_type"
-if [[ "$1" = "-r" && -n "$2" ]]; then
+if [[ "${1:-}" = "-r" && -n "${2:-}" ]]; then
     rel="$2"
 fi
 
-check_release $1 $2 $os
+check_release "${1:-}" "${2:-}" "$os"
 if [ $? -ne 0 ]; then
   return 1
 fi
 
 for ((i=1; i<=$#; i++)); do
     eval arg=\$$i
-    eval "argn=\${$((i+1))}"
+    eval "argn=\${$((i+1)):-}"
     case $arg in
         --help|-h)
             usage
@@ -352,7 +352,7 @@ for ((i=1; i<=$#; i++)); do
     esac
 done
 
-if [ -z "$compiler" ]; then
+if [ -z "${compiler:-}" ]; then
     if [ "$os" = "almalinux9" ]; then
         compiler="gcc14"
     elif [ "$os" = "ubuntu24" ]; then
@@ -368,7 +368,7 @@ fi
 
 k4path=$(/usr/bin/ls -rd /cvmfs/sw-nightlies.hsf.org/key4hep/releases/$rel/*$os*$compiler*$build_type | head -n1)
 
-if [ -n "$KEY4HEP_STACK" ]; then
+if [ -n "${KEY4HEP_STACK:-}" ]; then
     echo "The Key4hep software stack is already set up, please start a new shell to avoid conflicts"
     return 1
 fi
@@ -385,7 +385,7 @@ setup_actual=$(readlink -f $setup_script_path)
 export key4hep_stack_version=$(echo "$setup_actual"| grep -Po '(?<=key4hep-stack/)(.*)(?=-[[:alnum:]]{6}/)')
 
 # For SWAN
-if [ -n "$LCG_VERSION" ]; then
+if [ -n "${LCG_VERSION:-}" ]; then
     echo "A LCG stack has been sourced, unsetting the following variables to avoid conflicts:"
     echo "CMAKE_PREFIX_PATH CPPYY_BACKEND_LIBRARY LD_LIBRARY_PATH PKG_CONFIG_PATH PYTHONHOME PYTHONPATH"
     unset CMAKE_PREFIX_PATH CPPYY_BACKEND_LIBRARY LD_LIBRARY_PATH PKG_CONFIG_PATH PYTHONHOME PYTHONPATH
